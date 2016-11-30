bfb15220dc2f94fa6a1d537ac0b9c8066fa1938f
Canzea Technologies <aidan.cope@canzea.com>
2016-11-30 11:06:42 -0800

    Update filebeat

--- roles/monitoring/filebeat/configure.sh
+++ roles/monitoring/filebeat/configure.sh
@@ -1,4 +1,9 @@
 
 
-ruby ./init/template.rb roles/monitoring/filebeat/config/filebeat.yml /etc/filebeat/filebeat.yml
+
+# MOVE OVER THE CERTIFICATE TO EACH CLIENT SERVER
+ruby init/registry-key-value.rb certs/logstash | base64 --decode > /etc/pki/tls/certs/logstash-forwarder.crt
+
+
+ruby init/template.rb roles/monitoring/filebeat/config/filebeat.yml /etc/filebeat/filebeat.yml
 
--- roles/monitoring/filebeat/install.sh
+++ roles/monitoring/filebeat/install.sh
@@ -1,19 +1,6 @@
-# yes | rm -rf Configuration
 
-# git clone $GITLAB_URL/root/Configuration.git
+cp roles/monitoring/filebeat/config/elastic-beats.repo /etc/yum.repos.d/elastic-beats.repo
 
+rpm --import http://packages.elastic.co/GPG-KEY-elasticsearch
 
-#####################################################
-# ELK Client (Filebeat)
-#####################################################
-
-cp prod-1/Files/etc/yum.repos.d/elastic-beats.repo /etc/yum.repos.d/elastic-beats.repo
-
-sudo rpm --import http://packages.elastic.co/GPG-KEY-elasticsearch
-
-sudo yum -y install filebeat
-
-
-# MOVE OVER THE CERTIFICATE TO EACH CLIENT SERVER
-ruby init/registry-key-value.rb certs/logstash | base64 --decode > /etc/pki/tls/certs/logstash-forwarder.crt
-
+yum -y install filebeat
