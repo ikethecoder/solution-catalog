df9b78ae69151aa2dc23fd711ed0f0f52e94e8b5
Canzea Technologies <aidan.cope@canzea.com>
2016-12-03 01:29:17 -0800

    Updated git

--- roles/sourcecontrol/git/install.sh
+++ roles/sourcecontrol/git/install.sh
@@ -11,6 +11,8 @@ sudo -E -u git expect roles/application/user/key-gen.expect
 
 sudo -E -u root expect roles/application/user/key-gen.expect
 
+chmod +x roles/sourcecontrol/git/set-public-key.sh
+
 export TMP_PASSWORD=get1git; expect roles/sourcecontrol/git/set-public-key.expect
 
 chmod 600 /home/git/.ssh/authorized_keys
