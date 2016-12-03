d72d6a7ada28ff62dda58e90dc7966f1815b713a
Canzea Technologies <aidan.cope@canzea.com>
2016-12-03 00:06:08 -0800

    Updated nginx

--- roles/loadbalancer/nginx/configure.sh
+++ roles/loadbalancer/nginx/configure.sh
@@ -1,18 +1,9 @@
-yes | cp -v prod-1/Files/etc/nginx/nginx.conf /etc/nginx/nginx.conf
-yes | cp -v prod-1/Files/etc/nginx/conf.d/default.conf /etc/nginx/conf.d/default.conf
-
-# cp roles/loadbalancer/nginx/config/elastichq.conf /etc/nginx/conf.d/.
-# cp roles/loadbalancer/nginx/config/consul.conf /etc/nginx/conf.d/.
-
-#. ~/.bash_ike && ruby ./init/template.rb roles/loadbalancer/nginx/config/consul.conf /etc/nginx/conf.d/consul.conf
-# . ~/.bash_ike && ruby ./init/template.rb roles/loadbalancer/nginx/config/elastichq.conf /etc/nginx/conf.d/elastichq.conf
-
-# Workaround for having the web hooks work ok between gitlab and go.cd
-#. ~/.bash_ike && ruby ./init/template.rb roles/loadbalancer/nginx/config/gocd.conf /etc/nginx/conf.d/gocd.conf
+yes | cp -v roles/loadbalancer/nginx/config/nginx.conf /etc/nginx/nginx.conf
+yes | cp -v roles/loadbalancer/nginx/config/default.conf /etc/nginx/conf.d/default.conf
 
 mkdir -p /home/nginx/www
 
-yes | cp -v prod-1/Files/home/nginx/www/index.html /home/nginx/www/index.html
+yes | cp -v roles/loadbalancer/nginx/config/www/index.html /home/nginx/www/index.html
 
 htpasswd -c -db /etc/nginx/htpasswd.users admin admin1admin
 
