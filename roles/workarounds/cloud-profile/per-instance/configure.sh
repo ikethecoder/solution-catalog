89b843ef771e8b3f74c554c03c108125d717993b
Canzea Technologies <aidan.cope@canzea.com>
2016-12-03 03:37:43 -0800

    Updated consul

--- roles/workarounds/cloud-profile/per-instance/configure.sh
+++ roles/workarounds/cloud-profile/per-instance/configure.sh
@@ -9,6 +9,7 @@ export VAULT_TOKEN=abcd
 
 export HOSTNAME=`curl -s http://169.254.169.254/metadata/v1/hostname`
 export PUBLIC_IPV4=`curl -s http://169.254.169.254/metadata/v1/interfaces/public/0/ipv4/address`
+export PRIVATE_IPV4=`curl -s http://169.254.169.254/metadata/v1/interfaces/private/0/ipv4/address`
 
 chmod +x /opt/cloud-profile/configure.sh
 
