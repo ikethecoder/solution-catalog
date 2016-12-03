d8c4a01f46cd54556d7783c7304f59246fc28994
Canzea Technologies <aidan.cope@canzea.com>
2016-12-03 04:27:27 -0800

    Updated taiga

--- roles/agilepm/taiga-frontend/configure.sh
+++ roles/agilepm/taiga-frontend/configure.sh
@@ -1,5 +1,5 @@
 
-ruby ./init/template.rb roles/agilepm/taiga-frontend/config/conf.json /home/taiga/taiga-front-dist/dist/conf.json
+canzea --config_git_commit --template=roles/agilepm/taiga-frontend/config/conf.json /home/taiga/taiga-front-dist/dist/conf.json
 
 chown taiga:taiga /home/taiga/taiga-front-dist/dist/conf.json
 
