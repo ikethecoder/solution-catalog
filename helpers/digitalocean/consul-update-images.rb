16827b578faf37c3a18d898e5d5ce35214edffbd
Canzea Technologies <aidan.cope@canzea.com>
2016-12-06 15:46:55 -0800

    Updated digitalocean

--- helpers/digitalocean/consul-update-images.rb
+++ helpers/digitalocean/consul-update-images.rb
@@ -10,12 +10,15 @@ r = Registry.new
 base=parameters['base']
 instances=Integer(parameters['instances'])
 
-file = File.read("#{Canzea::config[:pwd]}/vps-#{base}-#{i}-image.json")
-response = JSON.parse(file)
+for i in 1..instances
 
-response.each do |item|
-    puts item['id']
+    file = File.read("#{Canzea::config[:pwd]}/vps-#{base}-#{i}-image.json")
+    response = JSON.parse(file)
 
-    r.register "images/#{base}-#{i}", 'id', item['id']
-    r.register "images/#{base}-#{i}", 'resource_id', item['resource_id']
+    response.each do |item|
+        puts item['id']
+
+        r.register "images/#{base}-#{i}", 'id', item['id']
+        r.register "images/#{base}-#{i}", 'resource_id', item['resource_id']
+    end
 end
