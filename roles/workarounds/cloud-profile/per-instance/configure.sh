b8dd73bbfdfa9e50fb16a1b46042a1fa30697845
Canzea Technologies <aidan.cope@canzea.com>
2016-12-13 14:56:03 -0800

    cloud-profile

--- roles/workarounds/cloud-profile/per-instance/configure.sh
+++ roles/workarounds/cloud-profile/per-instance/configure.sh
@@ -5,8 +5,8 @@ echo "Hello World.  The time is now $(date -R)!" | tee -a /root/output.txt
 (mkdir -p /root/bkup && mv /root/*.log /root/bkup)
 
 export HOME=/root
-export CONSUL_URL=http://10.136.0.48:8500
-export VAULT_TOKEN=abcd
+# export CONSUL_URL=http://10.136.0.48:8500
+# export VAULT_TOKEN=abcd
 
 export HOSTNAME=`curl -s http://169.254.169.254/metadata/v1/hostname`
 export PUBLIC_IPV4=`curl -s http://169.254.169.254/metadata/v1/interfaces/public/0/ipv4/address`
