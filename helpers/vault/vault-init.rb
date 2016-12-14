5a60dc46cfcda97a4a2452c43ffa22cc6e94af7a
Canzea Technologies <aidan.cope@canzea.com>
2016-12-14 14:41:18 -0800

    vault helper

--- helpers/vault/vault-init.rb
+++ helpers/vault/vault-init.rb
@@ -9,7 +9,20 @@ payload = { "secret_shares" => 1, "secret_threshold" => 1 }
 
 headers = {}
 
-http = Net::HTTP.new(ENV['VAULT_ADDRESS'], ENV['VAULT_PORT'])
+pemCert = File.read(ENV['VAULT_CLIENT_CERT'])
+pemKey = File.read(ENV['VAULT_CLIENT_KEY'])
+
+uri = URI.parse(ENV["VAULT_ADDR"] + '/v1/secret/' + svc['name'])
+http = Net::HTTP.new(uri.host, uri.port)
+http.use_ssl = true
+http.ca_file = ENV['VAULT_CACERT']
+http.cert = OpenSSL::X509::Certificate.new(pemCert)
+http.key = OpenSSL::PKey::RSA.new(pemKey)
+http.verify_mode = OpenSSL::SSL::VERIFY_PEER
+# http.set_debug_output($stdout)
+http.ssl_version = :SSLv23
+http = Net::HTTP.new(uri.request_uri)
+
 res = http.put("/v1/sys/init", payload.to_json, headers)
 
 puts res.body
