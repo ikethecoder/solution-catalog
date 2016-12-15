a90f7ede8424a74dfe4cc4d6a835175f6ed07844
Canzea Technologies <aidan.cope@canzea.com>
2016-12-15 11:11:12 -0800

    digitalocean helper

--- helpers/digitalocean-bootstrap/teardown.rb
+++ helpers/digitalocean-bootstrap/teardown.rb
@@ -1,33 +1,20 @@
 require 'droplet_kit'
 require 'json'
-require 'registry'
 require 'canzea/config'
 
 parameters = JSON.parse(ARGV[0])
 
-r = Registry.new
-
-base=parameters['base']
-instances=Integer(parameters['instances'])
-
-ary = []
-for i in 1..instances
-    id = i.to_s.rjust(2, '0')
-    ary.push(r.getValue "nodes/#{base}-#{id}", "id")
-end
+file = File.read("#{Canzea::config[:pwd]}/#{parameters['metadata']}")
+response = JSON.parse(file)
 
 token=ENV["DIGITALOCEAN_TOKEN"]
 
 client = DropletKit::Client.new(access_token: token)
 
-for id in ary
-    status = client.droplets.delete(id: id)
+response.each do |item|
+    puts item['name']
+
+    status = client.droplets.delete(id: item['id'])
 
     puts status.to_json
 end
-
-for i in 1..instances
-    id = i.to_s.rjust(2, '0')
-    r.deleteDirectory "nodes/#{base}-#{id}"
-end
-
