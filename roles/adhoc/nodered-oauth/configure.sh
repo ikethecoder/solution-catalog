409d1f0c85525f7f2bb61a55fae1b0391fe11a35
Canzea Technologies <aidan.cope@canzea.com>
2016-12-15 14:39:46 -0800

    nodered-oauth

--- roles/adhoc/nodered-oauth/configure.sh
+++ roles/adhoc/nodered-oauth/configure.sh
@@ -1,4 +1,10 @@
 
+su -l pm2user -c "mkdir -p /home/pm2user/oauth"
+
+su -l pm2user -c "(cd oauth && npm install oauth2-server express body-parser express-oauth-server node-red)"
+
+yes | cp -f roles/adhoc/nodered-oauth/lib/*.js /home/pm2user/oauth/.
+
 
 canzea --config_git_commit --template=roles/adhoc/nodered-oauth/config/nginx-ssl.conf /etc/nginx/conf.d/ssl.conf
 
--- roles/adhoc/nodered-oauth/install.sh
+++ roles/adhoc/nodered-oauth/install.sh
@@ -1,6 +1,1 @@
 
-su -l pm2user -c "mkdir -p /home/pm2user/oauth"
-
-su -l pm2user -c "(cd oauth && npm install oauth2-server express body-parser express-oauth-server node-red)"
-
-yes | cp -f roles/adhoc/nodered-oauth/lib/*.js /home/pm2user/oauth/.
