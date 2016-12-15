19021168f8bd94aa2530261bb1fe3cf23df1d5e9
Canzea Technologies <aidan.cope@canzea.com>
2016-12-15 13:17:53 -0800

    iptables

---
+++ roles/workarounds/iptables/configure.sh
@@ -0,0 +1,3 @@
+
+
+ruby roles/workarounds/iptables/strip.rb /etc/sysconfig/network-scripts/ifcfg-eth0 DNS
--- roles/workarounds/iptables/install.sh
+++ roles/workarounds/iptables/install.sh
@@ -4,6 +4,4 @@ sudo yum -y install iptables-services
 sudo systemctl enable iptables
 sudo systemctl enable ip6tables
 
-ruby roles/workarounds/iptables/strip.rb /etc/sysconfig/network-scripts/ifcfg-eth0 DNS
-
 
