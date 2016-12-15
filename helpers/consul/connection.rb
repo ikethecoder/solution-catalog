b7477466328962fcf8a92eb19ce47cdcea5f2593
Canzea Technologies <aidan.cope@canzea.com>
2016-12-14 16:56:09 -0800

    consul

--- helpers/consul/connection.rb
+++ helpers/consul/connection.rb
@@ -4,18 +4,21 @@ require 'openssl'
 
 class Connection
     def prepareHttpPutConnection()
-        pemCert = File.read(ENV['CONSUL_CLIENT_CERT'])
-            pemKey = File.read(ENV['CONSUL_CLIENT_KEY'])
-
         uri = URI.parse(ENV["CONSUL_ADDR"])
         http = Net::HTTP.new(uri.host, uri.port)
-        http.use_ssl = true
-        http.ca_file = ENV['CONSUL_CACERT']
-        http.cert = OpenSSL::X509::Certificate.new(pemCert)
-        http.key = OpenSSL::PKey::RSA.new(pemKey)
-        http.verify_mode = OpenSSL::SSL::VERIFY_PEER
-        # http.set_debug_output($stdout)
-        http.ssl_version = :SSLv23
+
+        if (ENV.has_key?('CONSUL_CLIENT_CERT'))
+            pemCert = File.read(ENV['CONSUL_CLIENT_CERT'])
+            pemKey = File.read(ENV['CONSUL_CLIENT_KEY'])
+
+            http.use_ssl = true
+            http.ca_file = ENV['CONSUL_CACERT']
+            http.cert = OpenSSL::X509::Certificate.new(pemCert)
+            http.key = OpenSSL::PKey::RSA.new(pemKey)
+            http.verify_mode = OpenSSL::SSL::VERIFY_PEER
+            # http.set_debug_output($stdout)
+            http.ssl_version = :SSLv23
+        end
         return http
     end
 end
