5a172c3db14ba2249536fd9915905c9366c004f6
Your Name <you@example.com>
2017-01-01 18:17:28 -0800

    Update vault

--- roles/workarounds/ecosystem-bootstrap/configure.sh
+++ roles/workarounds/ecosystem-bootstrap/configure.sh
@@ -4,6 +4,8 @@
 canzea --lifecycle=wire --solution=consul --action=add_keyvalue --args='{"key":"nodes/build-a-01/secure_host","value":"ese385.canzea.cc"}'
 
 #// Initialize and Unseal the vault
+#// export VAULT_URL=http://vault.service.dc1.consul:8200
+#// export VAULT_TOKEN=
 #// Inject the VAULT_URL and VAULT_TOKEN
 canzea --lifecycle=wire --solution=vault --action=vault-init-unseal --args='{}'
 
