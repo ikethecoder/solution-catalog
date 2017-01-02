ef4b334529f6d4bbb6ec48e67eb397471fb800f8
Your Name <you@example.com>
2017-01-01 19:31:05 -0800

    bootstrap

--- roles/workarounds/ecosystem-bootstrap/configure.sh
+++ roles/workarounds/ecosystem-bootstrap/configure.sh
@@ -2,18 +2,18 @@
 # HARDCODED DOMAIN NAME!
 
 #// Add the keyvalue for secure host
-canzea --lifecycle=wire --solution=consul --action=add_keyvalue --args='{"key":"nodes/build-a-01/secure_host","value":"es8851.canzea.cc"}'
+canzea --lifecycle=wire --solution=consul --action=add_keyvalue --args='{"key":"nodes/build-a-01/secure_host","value":"{{ECOSYSTEM}}.canzea.cc"}'
 
 #// Initialize and Unseal the vault
 #// Injects the VAULT_URL and VAULT_TOKEN (see /helpers/vault/environment.json)
 canzea --lifecycle=wire --solution=vault --action=vault-init-unseal --args='{}'
 
 
-# canzea --util=add-env VAULT_TOKEN token
+canzea --util=add-env VAULT_TOKEN 7e9d5883-549b-0644-32a9-335c127b1d68
 
 #// Add Digital Ocean secret
 canzea --lifecycle=wire --solution=vault --action=register-secret --args='{"key":"digitalocean","data":{"token":"{{DIGITALOCEAN_TOKEN}}"}}'
 
 #// DNS Registration
-canzea --lifecycle=wire --solution=digitalocean --action=register-dns --args='{"domain":"es8851","ip":"{{PUBLIC_IPV4}}","rootHost":"canzea.cc"}'
+canzea --lifecycle=wire --solution=digitalocean --action=register-dns --args='{"domain":"{{ECOSYSTEM}}","ip":"{{PUBLIC_IPV4}}","rootHost":"canzea.cc"}'
 
