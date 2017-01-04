e98d4be7e569d03fe08f56315314fb4dab2f1338
Your Name <you@example.com>
2017-01-04 14:26:15 -0800

    digitalocean bootstrap

---
+++ helpers/digitalocean-bootstrap/images.rb
@@ -0,0 +1,24 @@
+
+require 'droplet_kit'
+require 'json'
+require 'registry'
+require 'canzea/config'
+
+parameters = JSON.parse(ARGV[0])
+
+token = ENV["DIGITALOCEAN_TOKEN"]
+
+client = DropletKit::Client.new(access_token: token)
+
+output = []
+
+images = client.images.all(type: 'snapshot')
+images.each { | image |
+    if (image['type'] == "snapshot")
+        puts image['id']
+        puts image['type']
+        output.push ( {"id":image['id'], "name":image['name']} )
+    end
+}
+
+puts JSON.pretty_generate(output)
