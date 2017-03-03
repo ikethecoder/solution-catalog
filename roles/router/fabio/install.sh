434173641c5c162a1c2cceb09768f5cbad4a88a5
Your Name <you@example.com>
2017-03-03 00:15:05 -0800

    Updated fabio

--- roles/router/fabio/install.sh
+++ roles/router/fabio/install.sh
@@ -11,6 +11,8 @@ canzea --config_git_commit --template=roles/router/fabio/config/fabio.service /e
 
 
 # Fix a defect with the statsd plugin
+mkdir -p /opt/fabio/src/github.com/eBay/fabio/vendor/github.com/pubnub/go-metrics-statsd
+
 yes | cp -f roles/router/fabio/config/statsd.go /opt/fabio/src/github.com/eBay/fabio/vendor/github.com/pubnub/go-metrics-statsd/statsd.go
 
 (cd /opt/fabio/src/github.com/eBay/fabio && export GOPATH=/opt/fabio && go build)
