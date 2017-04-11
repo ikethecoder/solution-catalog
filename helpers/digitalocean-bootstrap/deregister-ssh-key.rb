d7bfdfbe59f3f425ccddbaec36a32fda587e0729
Your Name <you@example.com>
2017-04-11 08:39:42 -0700

    digitalocean-bootstrap deregister ssh key

---
+++ helpers/digitalocean-bootstrap/deregister-ssh-key.rb
@@ -0,0 +1,16 @@
+
+require 'droplet_kit'
+require 'json'
+
+parameters = JSON.parse(ARGV[0])
+
+token=ENV["DIGITALOCEAN_TOKEN"]
+
+client = DropletKit::Client.new(access_token: token)
+
+file = File.open('key-registration.json', "rb")
+pub_key = file.read
+
+response = client.ssh_keys.delete(id: pub_key['id']);
+
+
