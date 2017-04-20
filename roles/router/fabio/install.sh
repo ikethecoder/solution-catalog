93ad4c1a990ca2bd6a652c2bf0681e1877f24f41
Your Name <you@example.com>
2017-04-20 09:36:11 -0700

    Updated Fabio and golang (1.8) install

--- roles/router/fabio/install.sh
+++ roles/router/fabio/install.sh
@@ -1,12 +1,5 @@
 #yum -y install go
 
-# Remove any previous version
-rm -rf /usr/local/go
-
-wget https://storage.googleapis.com/golang/go1.8.1.linux-amd64.tar.gz
-
-tar -C /usr/local -xzf go1.8.1.linux-amd64.tar.gz
-
 
 mkdir -p /opt/fabio
 
--- roles/workarounds/golang/install.sh
+++ roles/workarounds/golang/install.sh
@@ -1,8 +1,11 @@
 
 yum -y erase go
 
-wget https://storage.googleapis.com/golang/go1.7.1.linux-amd64.tar.gz
+# Remove any previous version
+rm -rf /usr/local/go
 
-tar -C /usr/local -xzf go1.7.1.linux-amd64.tar.gz
+wget https://storage.googleapis.com/golang/go1.8.1.linux-amd64.tar.gz
+
+tar -C /usr/local -xzf go1.8.1.linux-amd64.tar.gz
 
 ln -s /usr/local/go/bin/go /usr/local/bin/go
