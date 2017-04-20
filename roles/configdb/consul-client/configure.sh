463274912ed9ac0eecebf14dcfee81a973cb8549
Your Name <you@example.com>
2017-04-20 11:32:52 -0700

    Added consul-client

---
+++ roles/configdb/consul-client/configure.sh
@@ -0,0 +1,6 @@
+
+canzea --config_git_commit --template=roles/configdb/consul-client/config/etc/systemd/system/multi-user.target.wants/consul.service /etc/systemd/system/multi-user.target.wants/consul.service
+
+
+canzea --config_git_commit --template=roles/configdb/consul-client/config/config.json /etc/consul.d/config.json
+
---
+++ roles/configdb/consul-client/install.sh
@@ -0,0 +1,27 @@
+
+
+wget -nv --no-cookies --no-check-certificate https://releases.hashicorp.com/consul/0.6.4/consul_0.6.4_linux_amd64.zip
+
+unzip -q -o consul_0.6.4_linux_amd64.zip
+
+cp consul /usr/local/bin/.
+
+yes | rm -f consul_0.6.4_linux_amd64.zip
+
+#// Binary execution rights and user running the daemon
+
+mkdir -p /opt/consul
+
+useradd -d /opt/consul -s /sbin/nologin consul
+
+chown -R consul:consul /opt/consul
+
+chmod -R 750 /opt/consul
+
+#// Configuration access rights
+
+mkdir -p /etc/consul.d
+
+chown root:consul /etc/consul.d
+
+chmod 750 /etc/consul.d
