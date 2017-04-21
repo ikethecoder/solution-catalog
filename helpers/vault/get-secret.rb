93f9ce00afcd7182ab93a7a932b482f77eafa496
Your Name <you@example.com>
2017-04-21 14:15:51 -0700

    vault register and get secret

--- helpers/vault/get-secret.rb
+++ helpers/vault/get-secret.rb
@@ -1,26 +1,11 @@
-# canzea --lifecycle=wire --solution=vault --action=register-secret --args='{"key":"digitalocean","data":{"token":"{{DIGITAL_OCEAN_API_KEY}}"}}'
-
 require 'json'
-require 'net/http'
 require 'template-runner'
-require_relative 'connection'
+require 'canzea/registry'
 
 parameters = JSON.parse(ARGV[0])
 
 key = parameters['key']
 
-http = Connection.new.prepareHttpPutConnection()
+r = Registry.new
 
-request = Net::HTTP::Get.new("/v1/secret/#{key}")
-request['Content-Type'] = 'application/json'
-request['X-Vault-Token'] = ENV['VAULT_TOKEN']
-
-res = http.request(request)
-
-if (Integer(res.code) != 200)
-   puts res.code
-   puts res.msg
-   raise("Unable to find secret #{res.code}")
-end
-
-puts res.body.to_json
+puts r.getSecret key
--- helpers/vault/register-secret.rb
+++ helpers/vault/register-secret.rb
@@ -1,9 +1,6 @@
-# canzea --lifecycle=wire --solution=vault --action=register-secret --args='{"key":"digitalocean","data":{"token":"{{DIGITAL_OCEAN_API_KEY}}"}}'
-
 require 'json'
-require 'net/http'
 require 'template-runner'
-require_relative 'connection'
+require 'canzea/registry'
 
 parameters = JSON.parse(ARGV[0])
 
@@ -15,17 +12,6 @@ payload.each_pair do |k, v|
     payload[k] = Template.new.processString(v, {})
 end
 
+r = Registry.new
 
-http = Connection.new.prepareHttpPutConnection()
-
-request = Net::HTTP::Post.new("/v1/secret/#{key}")
-request['Content-Type'] = 'application/json'
-request['X-Vault-Token'] = ENV['VAULT_TOKEN']
-
-res = http.request(request, payload.to_json)
-
-if (Integer(res.code) != 204)
-   puts res.code
-   puts res.msg
-   raise("Unable to register secret #{res.code}")
-end
+r.setSecret key, payload.to_json
