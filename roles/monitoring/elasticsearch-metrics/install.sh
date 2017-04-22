a4d3765f625ab6edcaaf968ddba7a606161fc00f
Your Name <you@example.com>
2017-04-22 09:27:49 -0700

    elasticsearch metrics for tracking stats in Grafana

---
+++ roles/monitoring/elasticsearch-metrics/install.sh
@@ -0,0 +1,16 @@
+
+
+git clone https://github.com/trevorndodds/elasticsearch-metrics
+
+mkdir /opt/elasticsearch-metrics
+
+cp -r elasticsearch-metrics /opt/elasticsearch-metrics/.
+
+rm -rf elasticsearch-metrics
+
+canzea --config_git_commit --template=roles/monitoring/elasticsearch-metrics/config/script.sh /opt/elasticsearch-metrics/bin/entrypoint.sh
+
+chmod +x /opt/elasticsearch-metrics/bin/entrypoint.sh
+
+canzea --config_git_commit --template=roles/monitoring/elasticsearch-metrics/config/elasticsearch-metrics.service /etc/systemd/system/multi-user.target.wants/elasticsearch-metrics.service
+
