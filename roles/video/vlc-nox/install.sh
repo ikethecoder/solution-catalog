a1db04fd4c68978b1de77fc2a9d367b2ae4a6e35
Your Name <you@example.com>
2017-04-28 08:52:49 -0700

    Updated fabio install

---
+++ roles/video/vlc-nox/install.sh
@@ -0,0 +1,9 @@
+
+
+yum localinstall http://download.videolan.org/vlc/0.8.6/SuSE/11.1/x86_64/vlc-noX-1.1.2-2.9.x86_64.rpm
+
+
+http://li.nux.ro/download/nux/dextop/el7/x86_64/nux-dextop-release-0-5.el7.nux.noarch.rpm
+
+yum clean all
+yum update
