9b5e828a8cbd1b5e89a9c20c7d243e997de46b03
Your Name <you@example.com>
2017-05-07 10:42:13 -0700

    Application Analyzer

--- helpers/application/analyze-project.rb
+++ helpers/application/analyze-project.rb
@@ -1,10 +1,11 @@
-# ruby t.rb '{"url":"https://gitlab.com/ikethecoder/hello-world-svc-app.git","branch":"master","name":"a"}'
+# canzea --lifecycle=wire --solution=application --action=analyze-project --args='{"sourceRepo":"https://gitlab.com/ikethecoder/hello-world-svc-app.git","branch":"master","name":"a"}'
 
 
 require 'git'
 require 'json'
 require 'xmlsimple'
 require 'java-properties'
+require_relative 'types/java-maven/java-maven-project'
 
 parameters = JSON.parse(ARGV[0])
 
@@ -14,16 +15,8 @@ name = parameters['name']
 
 g = Git.clone(url, name, :branch => branch, :path => '.')
 
-pom = XmlSimple.xml_in(name + "/pom.xml")
-
-groupId = pom['groupId'][0]
-artifactId = pom['artifactId'][0]
-version = pom['version'][0]
-
-content = {
-  "type" => "java-maven",
-  "groupId" => groupId,
-  "artifactId" => artifactId,
-  "version" => version
-}
-File.write("artifact-#{name}.json", JSON.generate(content))
+if (File.exists? "#{name}/pom.xml")
+    JavaMavenProject.new.createDetails name
+else
+    raise "Unable to identify project type"
+end
---
+++ helpers/application/types/java-maven/java-maven-project.rb
@@ -0,0 +1,22 @@
+require 'json'
+require 'xmlsimple'
+require 'java-properties'
+
+class JavaMavenProject
+
+    def createDetails(name)
+        pom = XmlSimple.xml_in(name + "/pom.xml")
+
+        groupId = pom['groupId'][0]
+        artifactId = pom['artifactId'][0]
+        version = pom['version'][0]
+
+        content = {
+          "type" => "java-maven",
+          "groupId" => groupId,
+          "artifactId" => artifactId,
+          "version" => version
+        }
+        File.write("artifact-#{name}.json", JSON.pretty_generate(content))
+    end
+end
