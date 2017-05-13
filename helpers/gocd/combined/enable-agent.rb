07edafc55ac3c2e980c0e685b4fab21ff7d7de9a
Your Name <you@example.com>
2017-05-13 12:50:06 -0700

    Go.CD Build pipeline

--- helpers/gocd/combined/enable-agent.rb
+++ helpers/gocd/combined/enable-agent.rb
@@ -1,3 +1,12 @@
 require_relative '../gocd-client'
 
-puts GoCDClient.findObject '4', 'agents', 'escd27-perf-app-01'
+cli = GoCDClient.new
+
+cli.findObject '4', 'agents', 'escd27-perf-app-01'
+
+
+json = cli.retrieveObject 'agents', 'escd27-perf-app-01'
+
+json['agent_config_state'] = 'Enabled'
+
+cli.putObject '4'', 'agents', 'escd27-perf-app-01', json
