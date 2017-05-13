cada09f17e6c621e8a11e700778dc8babdf395d0
Your Name <you@example.com>
2017-05-13 13:41:07 -0700

    Go.CD Agent Env

---
+++ helpers/gocd/combined/add-agent-to-env.rb
@@ -0,0 +1,25 @@
+# canzea --lifecycle=wire --solution=gocd --action=combined/add-agent-to-env --args='{"environment":"perf", ""agent":"escd27-perf-app-01"}'
+
+require 'json'
+require_relative '../gocd-client'
+
+parameters = JSON.parse(ARGV[0])
+
+env = parameters['environment']
+
+id = parameters['agent']
+
+cli = GoCDClient.new
+
+cli.findObject '4', 'agents', id
+
+json = cli.retrieveObject 'agents', id
+
+uuid = json['uuid']
+
+
+cli = GoCDClient.new '/go/api/admin'
+
+changes = "agents" : [ { "uuid" : uuid } ]
+
+cli.patchObject '2', 'environments', env, changes
