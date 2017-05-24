22bb5778750bda1b59467ef561683f44dbc23f92
Your Name <you@example.com>
2017-05-24 10:49:31 -0700

    Updated virtualmachine java installation

--- roles/virtualmachine/java/install.sh
+++ roles/virtualmachine/java/install.sh
@@ -2,9 +2,9 @@
 
 rm -f jdk-8u60-linux-x64.rpm
 
-wget -nv --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u60-b27/jdk-8u60-linux-x64.rpm"
+wget -nv -O jdk.rpm --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://javadl.oracle.com/webapps/download/AutoDL?BundleId=220304_d54c1d3a095b4ff2b6607d096fa80163"
 
-yum -y localinstall jdk-8u60-linux-x64.rpm
+yum -y localinstall jdk.rpm
 
-rm -f jdk-8u60-linux-x64.rpm
+rm -f jdk.rpm
 
