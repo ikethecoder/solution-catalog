5c63e52d7c86194fd572c311c4f6411664e440ec
Your Name <you@example.com>
2017-06-02 21:04:27 -0700

    Updated GOCD install

---
+++ helpers/gocd/combined/disable-agent.rb
@@ -0,0 +1,23 @@
+# canzea --lifecycle=wire --solution=gocd --action=combined/enable-agent --args='{"agent":"escd27-perf-app-01"}'
+
+require 'json'
+require_relative '../gocd-client'
+
+parameters = JSON.parse(ARGV[0])
+
+id = parameters['agent']
+
+cli = GoCDClient.new
+
+cli.findObject '4', 'agents', id
+
+
+json = cli.retrieveObject 'agents', id
+
+uuid = json['uuid']
+
+cli.getObject '4', 'agents', uuid
+
+changes = {'agent_config_state' => 'Disabled'}
+
+cli.patchObject '4', 'agents', uuid, changes
