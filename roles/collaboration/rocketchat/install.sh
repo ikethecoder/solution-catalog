2f7f33ee47d21db3145ccedbfebcab5fa4800cd6
Your Name <you@example.com>
2017-06-02 21:33:52 -0700

    Updated collaboration rocketchat install

--- roles/collaboration/rocketchat/install.sh
+++ roles/collaboration/rocketchat/install.sh
@@ -1,9 +1,9 @@
 
 # docker pull rocketchat/rocket.chat:latest
 
-docker create --name db -d mongo --smallfiles
+docker create --name db mongo --smallfiles
 
-docker create --name rocketchat --link db -p 8780:3000 -d rocket.chat
+docker create --name rocketchat --link db -p 8780:3000 rocket.chat
 
 # 0e241fa3dba894af93b2a4b5dde271137294257cbffd23a6b299e5a8c7be97aa
 
