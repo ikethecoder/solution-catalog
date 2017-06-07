b4016c4fb212e642723c02ffe5055ff64de9c154
Your Name <you@example.com>
2017-06-07 08:24:08 -0700

    Changed digitalocean helper for DNS to not use toolkit

--- helpers/digitalocean/register-dns.rb
+++ helpers/digitalocean/register-dns.rb
@@ -15,26 +15,26 @@ client = DOClient.new
 result = []
 
 payload = {
-            "type": "A",
-            "name": domain,
-            "data": ip,
-            "priority": null,
-            "port": null,
-            "ttl": 1800,
-            "weight": null
+            "type" => "A",
+            "name" => domain,
+            "data" => ip,
+            "priority" => null,
+            "port" => null,
+            "ttl" => 1800,
+            "weight" => null
           };
 
 response = client.post("domains/#{rootHost}/records", payload)
 result.push(response)
 
 payload = {
-            "type": "CNAME",
-            "name": "*.#{domain}",
-            "data": "#{domain}.#{rootHost}.",
-            "priority": null,
-            "port": null,
-            "ttl": 1800,
-            "weight": null
+            "type" => "CNAME",
+            "name" => "*.#{domain}",
+            "data" => "#{domain}.#{rootHost}.",
+            "priority" => null,
+            "port" => null,
+            "ttl" => 1800,
+            "weight" => null
           };
 response = client.post("domains/#{rootHost}/records", payload)
 result.push(response)
