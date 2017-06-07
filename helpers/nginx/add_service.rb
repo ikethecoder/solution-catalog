43f0700097328b6a17f3e0fabef5b64857071768
Your Name <you@example.com>
2017-06-07 14:36:18 -0700

    Updated add_service helper and put back phantomjs into ci

--- helpers/nginx/add_service.rb
+++ helpers/nginx/add_service.rb
@@ -1,5 +1,19 @@
 require 'json'
 require 'net/http'
+require 'template-runner'
+
 parameters = JSON.parse(ARGV[0])
 
+// parameters: location and passthrough
 
+t = Template.new
+
+partConfig = t.process "helpers/nginx/templates/service.templ", parameters
+
+sslConfig = File.read("/etc/nginx/conf.d/ssl.conf")
+
+index = sslConfig.rindex '}'
+
+sslConfig.insert index, partConfig
+
+puts sslConfig
