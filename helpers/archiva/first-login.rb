04ded8a4ec0d790fd043117ef0b5d1d3f20ca24a
Your Name <you@example.com>
2017-06-07 14:58:26 -0700

    Created archiva helper for first-login

---
+++ helpers/archiva/first-login.rb
@@ -0,0 +1,30 @@
+require "selenium-webdriver"
+
+wait = Selenium::WebDriver::Wait.new(:timeout => 5)
+
+driver = Selenium::WebDriver.for :phantomjs
+
+target_size = Selenium::WebDriver::Dimension.new(1024, 768)
+driver.manage.window.size = target_size
+
+driver.navigate.to "http://localhost:9080"
+
+sleep 5
+
+driver.find_element(:id, "create-admin-link-a").click
+
+sleep 5
+
+driver.find_element(:id, "password").send_keys "admin1admin1"
+driver.find_element(:id, "confirmPassword").send_keys "admin1admin1"
+
+driver.find_element(:id, "email").send_keys "aidan.cope@gmail.com"
+
+driver.find_element(:id, "validated").click
+
+driver.find_element(:id, "user-create-form-register-button").click
+
+sleep 5
+
+driver.quit
+
