1b29511fcc7ddb4329fed3447082e83c46bddbbf
Your Name <you@example.com>
2017-06-07 18:17:01 -0700

    node-red helper for doing auth

--- helpers/nodered/auth.rb
+++ helpers/nodered/auth.rb
@@ -9,7 +9,7 @@ out_file = parameters['out_file']
 # Get the authtoken and userId
 uri = URI(ENV['NODERED_URL'] + '/admin/auth/token')
 
-res = Net::HTTP.post_form(uri , 'client_id' => 'node-red-admin', 'grant_type' => 'password', 'scope' => '*', 'username' => 'admin', 'password' => 'password')
+res = Net::HTTP.post_form(uri , 'client_id' => 'thom', 'client_secret' => 'nightworld', 'grant_type' => 'password', 'scope' => '*', 'username' => 'thomseddon', 'password' => 'nightworld')
 
 data = JSON.parse(res.body)
 
