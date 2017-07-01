c6f0129ca21d5c48700d8f0d7172fbd943bcb0d0
Your Name <you@example.com>
2017-07-01 08:49:53 -0700

    Updated digitalocean bootstrap image creation

--- helpers/digitalocean-bootstrap/image-lookup.rb
+++ helpers/digitalocean-bootstrap/image-lookup.rb
@@ -8,25 +8,27 @@ token=ENV["DIGITALOCEAN_TOKEN"]
 
 client = DropletKit::Client.new(access_token: token)
 
-file = File.read("#{Canzea::config[:pwd]}/#{parameters['metadata']}")
-response = JSON.parse(file)
+name = parameters['name']
 
-response.each do |dropitem|
-    puts dropitem['name']
+file = File.read("#{Canzea::config[:pwd]}/vps-#{name}-image.json")
+item = JSON.parse(file)
+images = client.images.all(type: 'snapshot')
 
-    file = File.read("#{Canzea::config[:pwd]}/vps-#{dropitem['name']}-image.json")
-    item = JSON.parse(file)
-    images = client.images.all(type: 'snapshot')
+match = false
 
-    images.each { | image |
-        if (image['public'] == false and image['name'] == "#{dropitem['name']}-snapshot")
-            # puts "--" + "Potential match!"
-            # puts "--" + image['id']
-            # puts "--" + image['name']
-            # puts "--" + image['type']
+images.each { | image |
+    if (image['public'] == false and image['name'] == "#{name}-snapshot")
+        # puts "--" + "Potential match!"
+        # puts "--" + image['id']
+        # puts "--" + image['name']
+        # puts "--" + image['type']
 
-            File.open("#{Canzea::config[:pwd]}/vps-#{dropitem['name']}-snapshot-active.json", 'w') { |file| file.write(image.to_json) }
-            puts image.to_json
-        end
-    }
+        File.open("#{Canzea::config[:pwd]}/vps-#{name}-snapshot-active.json", 'w') { |file| file.write(image.to_json) }
+        puts image.to_json
+        match = true
+    end
+}
+
+if match == false
+    raise "Image not found"
 end
--- helpers/digitalocean-bootstrap/image.rb
+++ helpers/digitalocean-bootstrap/image.rb
@@ -8,39 +8,36 @@ token=ENV["DIGITALOCEAN_TOKEN"]
 
 client = DropletKit::Client.new(access_token: token)
 
-file = File.read("#{Canzea::config[:pwd]}/#{parameters['metadata']}")
-response = JSON.parse(file)
+name = parameters['name']
 
-response.each do |dropitem|
-    puts dropitem['name']
+puts name
 
-    did = dropitem['id']
+did = parameters['dropletId']
 
-    item = client.droplet_actions.snapshot(droplet_id: did, name: "#{dropitem['name']}-snapshot")
+item = client.droplet_actions.snapshot(droplet_id: did, name: "#{name}-snapshot")
 
-    File.open("#{Canzea::config[:pwd]}/vps-#{dropitem['name']}-image.json", 'w') { |file| file.write(item.to_json) }
+File.open("#{Canzea::config[:pwd]}/vps-#{name}-image.json", 'w') { |file| file.write(item.to_json) }
 
-    puts "#{item['id']} #{item['status']}"
+puts "#{item['id']} #{item['status']}"
 
-    file = File.read("#{Canzea::config[:pwd]}/vps-#{dropitem['name']}-image.json")
-    item = JSON.parse(file)
+file = File.read("#{Canzea::config[:pwd]}/vps-#{name}-image.json")
+item = JSON.parse(file)
 
-    active = false
-    while active == false
+active = false
+while active == false
 
-        puts "Looking up: #{item['id']}"
+    puts "Looking up: #{item['id']}"
 
-        image = client.droplet_actions.find(droplet_id: item['resource_id'], id: item['id'])
+    image = client.droplet_actions.find(droplet_id: item['resource_id'], id: item['id'])
 
-        puts image.to_json
+    puts image.to_json
 
-        if image.status == "in-progress"
-            sleep(10.seconds)
-        else
-            active = true
+    if image.status == "in-progress"
+        sleep(10.seconds)
+    else
+        active = true
 
-            File.open("#{Canzea::config[:pwd]}/vps-#{dropitem['name']}-image-active.json", 'w') { |file| file.write(image.to_json) }
+        File.open("#{Canzea::config[:pwd]}/vps-#{name}-image-active.json", 'w') { |file| file.write(image.to_json) }
 
-        end
     end
 end
