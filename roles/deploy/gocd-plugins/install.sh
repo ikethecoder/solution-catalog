6a4f6c497efcebd53212eb84b5d9d23a76b6f87a
Your Name <you@example.com>
2017-07-31 11:19:31 -0700

    GoCD Server might not have the plugins folder created so create if it hasn't been.

--- roles/deploy/gocd-plugins/install.sh
+++ roles/deploy/gocd-plugins/install.sh
@@ -3,5 +3,7 @@
 # https://github.com/matt-richardson/gocd-websocket-notifier/releases/download/0.4.2%2Bbuild.60/gocd-websocket-notifier.jar
 
 
+mkdir -p /var/lib/go-server/plugins/external
+
 (cd /var/lib/go-server/plugins/external && wget https://github.com/matt-richardson/gocd-websocket-notifier/releases/download/0.4.2%2Bbuild.60/gocd-websocket-notifier.jar)
 
