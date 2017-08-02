eeea4f0c2ffcb715afd4f446695f703292e4d7a1
Your Name <you@example.com>
2017-08-01 18:53:45 -0700

    nginx helpers Standardize the parameters

--- helpers/nginx/ssl-route.sh
+++ helpers/nginx/ssl-route.sh
@@ -1,3 +1,23 @@
 
-(cd $CATALOG_LOCATION && canzea --lifecycle=wire --solution=nginx --action=add_service --args='{"location":"/sc/", "passthrough":"http://localhost:851/"}')
+while [[ $# -gt 1 ]]
+do
+  key="$1"
+
+  echo "$key = $2"
+
+  case $key in
+    --location)
+    export LOCATION="$2"
+    shift
+    ;;
+    --passthrough)
+    export PASSTHROUGH="$2"
+    shift
+    ;;
+  esac
+
+  shift
+done
+
+(cd $CATALOG_LOCATION && canzea --lifecycle=wire --solution=nginx --action=add_service --args='{"location":"{{LOCATION}}", "passthrough":"{{PASSTHROUGH}}"}')
 
