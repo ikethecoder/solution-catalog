4c06198f2777b58e92fdad4711d90065977b97de
Your Name <you@example.com>
2017-08-03 16:15:41 -0700

    gocd upgraded to 17.3.0

--- roles/deploy/gocd/install.sh
+++ roles/deploy/gocd/install.sh
@@ -3,10 +3,8 @@
 
 # https://download.gocd.io/binaries/17.3.0-4704/rpm/go-server-17.3.0-4704.noarch.rpm
 
-# GOCD-PLUGINS is failing when we use this version.
-# wget https://download.gocd.io/binaries/17.3.0-4704/rpm/go-server-17.3.0-4704.noarch.rpm
-# yum -y localinstall go-server-17.3.0-4704.noarch.rpm
+wget https://download.gocd.io/binaries/17.3.0-4704/rpm/go-server-17.3.0-4704.noarch.rpm
+yum -y localinstall go-server-17.3.0-4704.noarch.rpm
 
-wget https://download.gocd.io/binaries/16.12.0-4352/rpm/go-server-16.12.0-4352.noarch.rpm
-
-yum -y localinstall go-server-16.12.0-4352.noarch.rpm
+# wget https://download.gocd.io/binaries/16.12.0-4352/rpm/go-server-16.12.0-4352.noarch.rpm
+# yum -y localinstall go-server-16.12.0-4352.noarch.rpm
