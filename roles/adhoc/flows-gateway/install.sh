86011bccb0cb37de1ff2e3b1eb519404e57e1a37
Your Name <you@example.com>
2017-08-04 09:17:35 -0700

    flows-gateway / using custom elasticsearch plugin

--- roles/adhoc/flows-gateway/install.sh
+++ roles/adhoc/flows-gateway/install.sh
@@ -2,13 +2,21 @@ su -l pm2user -c "mkdir -p /home/pm2user/flows-gateway"
 
 su -l pm2user -c "(cd flows-gateway && npm install flows-gateway)"
 
-su -l pm2user -c "(cd flows-gateway && npm install node-red-contrib-http-request node-red-dashboard node-red-nodes node-red-contrib-graphs node-red-contrib-influxdb node-red-contrib-selenium-webdriver node-red-contrib-bigtimer node-red-contrib-cron node-red-contrib-bigexec node-red-node-mongodb node-red-contrib-postgres node-red-contrib-elasticsearch3 node-red-contrib-json-schema node-red-contrib-chatbot node-red-contrib-counter)"
+su -l pm2user -c "(cd flows-gateway && npm install node-red-contrib-http-request node-red-dashboard node-red-nodes node-red-contrib-graphs node-red-contrib-influxdb node-red-contrib-selenium-webdriver node-red-contrib-bigtimer node-red-contrib-cron node-red-contrib-bigexec node-red-node-mongodb node-red-contrib-postgres node-red-contrib-json-schema node-red-contrib-chatbot node-red-contrib-counter)"
 
 yes | cp -f roles/adhoc/flows-gateway/lib/* /home/pm2user/flows-gateway/.
 
 
 # Custom biglib that passes on the req/res context objects
 
+
+
 su -l pm2user -c "(cd flows-gateway && git clone https://github.com/ikethecoder/node-red-biglib.git)"
 
 su -l pm2user -c "(cd flows-gateway && npm install ./node-red-biglib)"
+
+# Custom elasticsearch
+
+su -l pm2user -c "(cd flows-gateway && git clone https://github.com/ikethecoder/node-red-contrib-canzea-elasticsearch.git)"
+
+su -l pm2user -c "(cd flows-gateway && npm install ./node-red-contrib-canzea-elasticsearch)"
