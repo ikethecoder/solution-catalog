f3408e3251d8f53f1ed14a2777b161bf4aeb4180
ikethecoder <ikethecoder@canzea.com>
2017-08-17 20:11:19 -0700

    helper for docker service

---
+++ helpers/application/configure-service.rb
@@ -0,0 +1,14 @@
+# canzea --lifecycle=wire --solution=application --action=install-app --args='{"type":"js-npm", "project":"console-ui","port":5505}'
+
+require 'json'
+require 'net/http'
+require 'template-runner'
+require 'canzea/config'
+require_relative 'install-project'
+
+parameters = JSON.parse(ARGV[0])
+
+catalog = ENV['CATALOG_LOCATION']
+
+t = Template.new
+t.processAndWriteToFile "#{catalog}/roles/application/conf/docker.service", "/opt/applications/static/#{container}/#{container}.service", parameters
