d011d218cd89b78cc71e43e51fc8f1cc72df7965
ikethecoder <ikethecoder@canzea.com>
2017-10-05 06:21:19 -0700

    Tweaked

--- helpers/ecosystem_catalog/plus.rb
+++ helpers/ecosystem_catalog/plus.rb
@@ -6,4 +6,8 @@ g = Git.open(ENV['CATALOG_LOCATION'])
 branch = g.current_branch
 commit = g.log[0]
 
-puts JSON.generate({"branch" => branch, "commitRevision" => commit, "date" => commit.date.strftime('%FT%T%:z'), "message" => commit.message, "author" => {"name" => commit.author.name, "email" => commit.author.email} })
+response = JSON.generate({"branch" => branch, "commitRevision" => commit, "date" => commit.date.strftime('%FT%T%:z'), "message" => commit.message, "author" => {"name" => commit.author.name, "email" => commit.author.email} })
+puts response
+
+File.open('info.json', 'w') { |file| file.write(response) }
+
