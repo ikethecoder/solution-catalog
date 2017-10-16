2c729552bba2e550b8b2967d4fd7d9919c2f95b2
ikethecoder <ikethecoder@canzea.com>
2017-10-16 13:29:23 -0700

    Virtualmachine JDK - replaced wget with curl

--- roles/virtualmachine/jdk/install.sh
+++ roles/virtualmachine/jdk/install.sh
@@ -1,6 +1,6 @@
 
 rm -f jdk.rpm
 
-wget -nv -O jdk.rpm --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u144-b01/090f390dda5b47b9b721c7dfaa008135/jdk-8u144-linux-x64.rpm"
+curl -s -L -o jdk.rpm -H "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u144-b01/090f390dda5b47b9b721c7dfaa008135/jdk-8u144-linux-x64.rpm
 
 yum -y localinstall jdk.rpm
