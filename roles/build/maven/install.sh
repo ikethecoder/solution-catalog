ccc32613d0f9eb65c6a577a8bd05df4130979cf7
ikethecoder <ikethecoder@canzea.com>
2017-10-16 13:34:57 -0700

    Updated maven install - wget to curl

--- roles/build/maven/install.sh
+++ roles/build/maven/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget http://repos.fedorapeople.org/repos/dchen/apache-maven/epel-apache-maven.repo -O /etc/yum.repos.d/epel-apache-maven.repo
+curl -s http://repos.fedorapeople.org/repos/dchen/apache-maven/epel-apache-maven.repo -o /etc/yum.repos.d/epel-apache-maven.repo
 
 yum -y install apache-maven
 
