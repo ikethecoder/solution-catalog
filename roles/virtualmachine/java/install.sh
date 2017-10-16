a57c39141bce95816175ecea25caf030860f2cd4
ikethecoder <ikethecoder@canzea.com>
2017-10-16 13:31:26 -0700

    Updated java install - wget to curl

--- roles/virtualmachine/java/install.sh
+++ roles/virtualmachine/java/install.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-rm -f jdk-8u60-linux-x64.rpm
+rm -f jdk.rpm
 
-wget -nv -O jdk.rpm --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://javadl.oracle.com/webapps/download/AutoDL?BundleId=220304_d54c1d3a095b4ff2b6607d096fa80163"
+curl -s -L -o jdk.rpm -H "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://javadl.oracle.com/webapps/download/AutoDL?BundleId=220304_d54c1d3a095b4ff2b6607d096fa80163"
 
 yum -y localinstall jdk.rpm
 
 rm -f jdk.rpm
+
+
+
 
