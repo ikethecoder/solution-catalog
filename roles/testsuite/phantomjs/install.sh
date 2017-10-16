ae85e5890e46fc0d239ccea09a3612dbb2820fbb
ikethecoder <ikethecoder@canzea.com>
2017-10-16 13:40:10 -0700

    Install phantomjs - wget to curl

--- roles/testsuite/phantomjs/install.sh
+++ roles/testsuite/phantomjs/install.sh
@@ -4,7 +4,7 @@ yum -y install bzip2
 echo "THIS DOES NOT ALWAYS WORK!!!"
 
 # wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2
-wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2
+curl -L -s -O https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2
 
 tar xvf phantomjs-2.1.1-linux-x86_64.tar.bz2
 
