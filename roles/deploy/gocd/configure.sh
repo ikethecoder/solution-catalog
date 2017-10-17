af3387768d94c9961b9d44c56697c4ff4f3e16e6
ikethecoder <ikethecoder@canzea.com>
2017-10-16 20:37:41 -0700

    GoCD configure - remove nginx config

--- roles/deploy/gocd/configure.sh
+++ roles/deploy/gocd/configure.sh
@@ -3,4 +3,4 @@
 yes | cp -f roles/deploy/gocd/config/go-server /etc/default/go-server
 
 
-canzea --config_git_commit --template=roles/deploy/gocd/config/nginx/gocd.conf /etc/nginx/conf.d/gocd.conf
+# canzea --config_git_commit --template=roles/deploy/gocd/config/nginx/gocd.conf /etc/nginx/conf.d/gocd.conf
