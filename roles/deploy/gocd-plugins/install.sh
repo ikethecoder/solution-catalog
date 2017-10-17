21aae3d6a93dfe6e1cd1b3b99c3ec58b1e636fbb
ikethecoder <ikethecoder@canzea.com>
2017-10-17 09:49:28 -0700

    Make sure gocd server plugins has correct user

--- roles/deploy/gocd-plugins/install.sh
+++ roles/deploy/gocd-plugins/install.sh
@@ -7,3 +7,5 @@ mkdir -p /var/lib/go-server/plugins/external
 
 (cd /var/lib/go-server/plugins/external && curl -L -s -O https://github.com/matt-richardson/gocd-websocket-notifier/releases/download/0.4.2%2Bbuild.60/gocd-websocket-notifier.jar)
 
+chown -R go:go /var/lib/go-server/plugins
+
