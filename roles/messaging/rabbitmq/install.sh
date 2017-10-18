208632ee92c1e7371580bed9e8fd7691ca13ea62
ikethecoder <ikethecoder@canzea.com>
2017-10-17 21:37:25 -0700

    RabbitMQ update wget to curl

--- roles/messaging/rabbitmq/install.sh
+++ roles/messaging/rabbitmq/install.sh
@@ -2,7 +2,7 @@
 
 yum -y install erlang
 
-wget -nv http://www.rabbitmq.com/releases/rabbitmq-server/v3.6.0/rabbitmq-server-3.6.0-1.noarch.rpm
+curl -L -s -O http://www.rabbitmq.com/releases/rabbitmq-server/v3.6.0/rabbitmq-server-3.6.0-1.noarch.rpm
 
 rpm --import https://www.rabbitmq.com/rabbitmq-signing-key-public.asc
 
