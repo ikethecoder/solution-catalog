06522a2f747cc15db1730dd58a38c093d71ab17d
ikethecoder <ikethecoder@canzea.com>
2017-10-19 11:25:52 -0700

    Update config-gogs

--- helpers/ecosystem/config-gogs.rb
+++ helpers/ecosystem/config-gogs.rb
@@ -3,6 +3,7 @@ require 'fileutils'
 require 'commands/push-config'
 
 
+system("canzea --util=add-env ECOSYSTEM_CONFIG_GIT git@165.227.87.135:root/ecosystem.git")
 
 # result = exec("curl -q -v -u root:admin1admin http://165.227.87.135:10080/api/v1/users/root/tokens -X POST -d '{\"name\":\"gogs-2\"}' -H 'Content-Type: application/json'"
 
