b8f464ac2968f97f3d4283dc332020cfed200992
ikethecoder <ikethecoder@canzea.com>
2017-10-19 11:05:25 -0700

    config-gogs

---
+++ helpers/ecosystem/config-gogs.rb
@@ -0,0 +1,11 @@
+require 'json'
+require 'fileutils'
+require 'commands/push-config'
+
+
+
+result = exec("curl -q -v -u root:admin1admin http://165.227.87.135:10080/api/v1/users/root/tokens -X POST -d '{\"name\":\"gogs-2\"}' -H 'Content-Type: application/json'"
+
+token = JSON.parse(result)['sha1']
+
+puts token
