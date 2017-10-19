c6c06276a71829c29a425e48f240d8ed6a83db8c
ikethecoder <ikethecoder@canzea.com>
2017-10-19 11:11:28 -0700

    Updated builder terraform template

--- helpers/ecosystem/config-gogs.rb
+++ helpers/ecosystem/config-gogs.rb
@@ -4,8 +4,8 @@ require 'commands/push-config'
 
 
 
-result = exec("curl -q -v -u root:admin1admin http://165.227.87.135:10080/api/v1/users/root/tokens -X POST -d '{\"name\":\"gogs-2\"}' -H 'Content-Type: application/json'"
+# result = exec("curl -q -v -u root:admin1admin http://165.227.87.135:10080/api/v1/users/root/tokens -X POST -d '{\"name\":\"gogs-2\"}' -H 'Content-Type: application/json'"
 
-token = JSON.parse(result)['sha1']
+# token = JSON.parse(result)['sha1']
 
-puts token
+# puts token
