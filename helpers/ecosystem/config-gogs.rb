940f06075c80e8d660a31d730481c856fb291d8d
ikethecoder <ikethecoder@canzea.com>
2017-10-19 11:44:57 -0700

    Config gogs helper update

--- helpers/ecosystem/config-gogs.rb
+++ helpers/ecosystem/config-gogs.rb
@@ -3,6 +3,11 @@ require 'fileutils'
 require 'commands/push-config'
 
 
+config = "Host 165.227.87.135\n   Port 10022"
+File.write("/root/.ssh/config", config)
+
+system("ssh-keyscan -p 10022 165.227.87.135 >> ~/.ssh/known_hosts")
+
 system("canzea --util=add-env ECOSYSTEM_CONFIG_GIT git@165.227.87.135:root/ecosystem.git")
 
 # result = exec("curl -q -v -u root:admin1admin http://165.227.87.135:10080/api/v1/users/root/tokens -X POST -d '{\"name\":\"gogs-2\"}' -H 'Content-Type: application/json'"
