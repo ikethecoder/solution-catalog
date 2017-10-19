54331174353d7f17fb90c63531acd71b26a962e2
ikethecoder <ikethecoder@canzea.com>
2017-10-19 13:03:21 -0700

    Update helper ecosystem analyze

--- helpers/ecosystem/analyze.rb
+++ helpers/ecosystem/analyze.rb
@@ -14,13 +14,3 @@ folder = '/root'
 n.run "(cd #{folder}; yum list > yum-list.txt)", 0, 0
 
 n.run "(cd #{folder}; gem list > gem-list.txt)", 0, 0
-
-es = ENV["ECOSYSTEM"]
-hostname = ENV['HOSTNAME']
-
-files = [
-  {"file" => "gem-list.txt", "path" => "ecosystems/#{es}/instances/#{hostname}/gem-list.txt"},
-  {"file" => "yum-list.txt", "path" => "ecosystems/#{es}/instances/#{hostname}/yum-list.txt"}
-]
-
-PushConfig.new.do ENV['ECOSYSTEM_CONFIG_GIT'], "Analyze", { "files" => files }
