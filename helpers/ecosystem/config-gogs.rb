4f04df71fcdeba9e7c2be897885df3405abe75e8
ikethecoder <ikethecoder@canzea.com>
2017-10-19 12:33:25 -0700

    Updated ecosystem config-gogs

--- helpers/ecosystem/config-gogs.rb
+++ helpers/ecosystem/config-gogs.rb
@@ -2,16 +2,11 @@ require 'json'
 require 'fileutils'
 require 'commands/push-config'
 
+ADDR = ENV['GOGS_ADDRESS']
 
-config = "Host 165.227.87.135\n   Port 10022"
+config = "Host #{ADDR}\n   Port 10022"
 File.write("/root/.ssh/config", config)
 
-system("ssh-keyscan -p 10022 165.227.87.135 >> ~/.ssh/known_hosts")
+system("ssh-keyscan -p 10022 #{ADDR} >> ~/.ssh/known_hosts")
 
-system("canzea --util=add-env ECOSYSTEM_CONFIG_GIT git@165.227.87.135:root/ecosystem.git")
-
-# result = exec("curl -q -v -u root:admin1admin http://165.227.87.135:10080/api/v1/users/root/tokens -X POST -d '{\"name\":\"gogs-2\"}' -H 'Content-Type: application/json'"
-
-# token = JSON.parse(result)['sha1']
-
-# puts token
+system("canzea --util=add-env ECOSYSTEM_CONFIG_GIT git@#{ADDR}:root/ecosystem.git")
