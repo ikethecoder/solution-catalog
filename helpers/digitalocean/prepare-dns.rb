0740b5a864deae40c815eff35a45f433364b47a3
Ike The Coder <ikethecoder@canzea.com>
2017-10-25 10:58:16 -0700

    digitalocean dns

--- helpers/digitalocean/prepare-dns.rb
+++ helpers/digitalocean/prepare-dns.rb
@@ -1,14 +1,18 @@
 require 'json'
 require 'fileutils'
+require 'template-runner'
 require 'commands/push-config'
 
 params = JSON.parse(ARGV[0])
 
 pc = PushConfig.new
 
-buffer = File.read("#{ENV['CATALOG_LOCATION']}/helpers/digitalocean/templates/dns.templ")
+t = Template.new
+
+template = "#{ENV['CATALOG_LOCATION']}/helpers/digitalocean/templates/dns.templ"
+
+buffer = t.process template, params)
 
 pc.write "terraform/#{params['domain']}.tf", buffer
 
-pc.commit "terraform"
-
+pc.commit "terraform for #{params['domain']}"
