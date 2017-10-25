bb828cb6403935b396ebbcc8e7bf0db606fdd001
Ike The Coder <ikethecoder@canzea.com>
2017-10-25 16:17:19 -0700

    flows upgrade helper

--- helpers/nodered/upgrade-flows.sh
+++ helpers/nodered/upgrade-flows.sh
@@ -1,5 +1,5 @@
 
-(cd ~/.ecosystem-catalog/catalog && unzip blueprints/neat-and-tidy/flows/flows.zip)
+(cd ~/.ecosystem-catalog/catalog && unzip blueprints/neat-and-tidy/flows/flows-8c020b4d77297af5bb34afeb0f1f2311.zip)
 
 (cd ~/.ecosystem-catalog/catalog && canzea --lifecycle=wire --solution=nodered --action=flows --args='{"file":"nr-tab-id-global.flow"}')
 
