21c6bd121e0f781fb517a1521635d364c94701f6
Ike The Coder <ikethecoder@canzea.com>
2017-10-26 18:51:22 -0700

    Changed config gogs

--- helpers/ecosystem/config-gogs.rb
+++ helpers/ecosystem/config-gogs.rb
@@ -9,4 +9,4 @@ File.write("/root/.ssh/config", config)
 
 system("ssh-keyscan -p 10022 #{ADDR} >> ~/.ssh/known_hosts")
 
-system("canzea --util=add-env ECOSYSTEM_CONFIG_GIT git@#{ADDR}:root/ecosystem.git")
+system("canzea --util=add-env ECOSYSTEM_CONFIG_GIT git@#{ADDR}:root/ecosystems.git")
