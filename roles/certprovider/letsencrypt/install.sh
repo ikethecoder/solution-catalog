0c6f96a76b0b66543408417d642f1327c3894804
Ike The Coder <ikethecoder@canzea.com>
2017-11-02 22:58:03 -0700

    letsencrypt

--- roles/certprovider/letsencrypt/install.sh
+++ roles/certprovider/letsencrypt/install.sh
@@ -3,7 +3,7 @@
 
 (cd ~ && git clone https://github.com/letsencrypt/letsencrypt)
 
-sudo openssl dhparam -out /etc/ssl/certs/dhparam.pem 2048
+openssl dhparam -out /etc/ssl/certs/dhparam.pem 2048
 
 # Stop NGINX
 # Add DNS entry so that dev.ikethecoder.io resolves
