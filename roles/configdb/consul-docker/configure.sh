e53c2c329a1a5dc458ef87dc147bc604aa81b1c2
Ike The Coder <ikethecoder@canzea.com>
2017-11-06 09:45:42 -0800

    consul-docker

--- roles/configdb/consul-docker/configure.sh
+++ roles/configdb/consul-docker/configure.sh
@@ -1,13 +1,13 @@
 
 canzea --config_git_commit --template=roles/configdb/consul-docker/config/consul.service /etc/systemd/system/multi-user.target.wants/consul.service
 
-(cd /etc/consul.d/ssl/CA && openssl req -newkey rsa:1024 -nodes -out consul.csr -keyout consul.key -subj "/C=US/ST=Denial/L=Springfield/O=Canzea/CN=consul.service.dc1.consul")
+(cd /var/local/consul/ssl/CA && openssl req -newkey rsa:1024 -nodes -out consul.csr -keyout consul.key -subj "/C=US/ST=Denial/L=Springfield/O=Canzea/CN=consul.service.dc1.consul")
 
-(cd /etc/consul.d/ssl/CA && openssl ca -batch -config myca.conf -notext -in consul.csr -out consul.cert)
+(cd /var/local/consul/ssl/CA && openssl ca -batch -config myca.conf -notext -in consul.csr -out consul.cert)
 
-cp /etc/consul.d/ssl/CA/consul.* /etc/consul.d/ssl/.
-cp /etc/consul.d/ssl/CA/ca.cert /etc/consul.d/ssl/.
-rm /etc/consul.d/ssl/consul.csr
+cp /var/local/consul/ssl/CA/consul.* /var/local/consul/ssl/.
+cp /var/local/consul/ssl/CA/ca.cert /var/local/consul/ssl/.
+rm /var/local/consul/ssl/consul.csr
 
 canzea --util=add-env CONSUL_URL "https://consul.service.dc1.consul:8080"
 
--- roles/configdb/consul-docker/install.sh
+++ roles/configdb/consul-docker/install.sh
@@ -1,10 +1,12 @@
 
+mkdir -p /opt/cloud-profile
+
 mkdir -p /var/local/consul
 chown centos:users /var/local/consul
 
 roles/configdb/consul-docker/tls.sh
 
-docker build --tag consul roles/configdb/consul-docker/config/Dockerfile
+docker build --tag consul roles/configdb/consul-docker/config
 
-(export HOST_IP=10.136.23.135 && docker create --name consul --net=host -v /var/local/consul/data/consul.d:/consul/data -v /var/local/consul/ssl:/etc/consul.d/ssl -e 'CONSUL_ALLOW_PRIVILEGED_PORTS=' $SERVICE agent -bind=$HOST_IP -dns-port=53 -recursor=8.8.8.8 -retry-join=$HOST_IP -client=$HOST_IP -bootstrap-expect=2 -server)
+(export HOST_IP=10.136.23.135 && docker create --name consul --net=host -v /var/local/consul/data/consul.d:/consul/data -v /var/local/consul/ssl:/etc/consul.d/ssl -e 'CONSUL_ALLOW_PRIVILEGED_PORTS=' consul agent -bind=$HOST_IP -dns-port=53 -recursor=8.8.8.8 -retry-join=$HOST_IP -client=$HOST_IP -bootstrap-expect=2 -server)
 
