2d6fd0065e46d98bbef6b6ce4ac0d71a87965daa
Ike The Coder <ikethecoder@canzea.com>
2017-11-06 11:27:31 -0800

    Phantomjs docker

---
+++ roles/testsuite/phantomjs-docker/install.sh
@@ -0,0 +1,14 @@
+
+
+
+
+docker create --name phantomjs -p 8001:8001 -v /var/local/consul/ssl:/etc/consul.d/ssl canzea/phantomjs:latest
+
+
+canzea --config_git_commit --template=roles/testsuite/phantomjs-docker/config/phantomjs.service /etc/systemd/system/multi-user.target.wants/phantomjs.service
+
+
+systemctl daemon-reload
+systemctl start phantomjs
+
+
