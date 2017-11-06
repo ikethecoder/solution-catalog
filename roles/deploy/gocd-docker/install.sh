9c34d05765fd6edbbf35754c17d695ea642b7bbc
Ike The Coder <ikethecoder@canzea.com>
2017-11-06 11:33:12 -0800

    gocd docker

--- roles/deploy/gocd-docker/install.sh
+++ roles/deploy/gocd-docker/install.sh
@@ -1,9 +1,8 @@
 
 
+docker build --tag gocd_local roles/deploy/gocd-docker/.
 
-
-docker create --name gocd -p 8153:8153 -v /var/local/consul/ssl:/etc/consul.d/ssl canzea/gocd:latest
-
+docker create --name gocd -p 8153:8153 -v /var/local/consul/ssl:/etc/consul.d/ssl gocd_local
 
 canzea --config_git_commit --template=roles/deploy/gocd-docker/config/gocd.service /etc/systemd/system/multi-user.target.wants/gocd.service
 
