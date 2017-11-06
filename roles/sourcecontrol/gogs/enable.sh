7d389138fa8d103d16eb06870ce633edd2319130
Ike The Coder <ikethecoder@canzea.com>
2017-11-06 11:45:02 -0800

    Gogs update

--- roles/sourcecontrol/gogs/enable.sh
+++ roles/sourcecontrol/gogs/enable.sh
@@ -1,8 +1,8 @@
 yes | cp -f roles/sourcecontrol/gogs/config/gogs.service /etc/systemd/system/multi-user.target.wants/.
 
-mkdir -p /var/gogs/gogs/conf
+mkdir -p /var/local/gogs/gogs/conf
 
-yes | cp -f roles/sourcecontrol/gogs/config/app.ini /var/gogs/gogs/conf/app.ini
+yes | cp -f roles/sourcecontrol/gogs/config/app.ini /var/local/gogs/gogs/conf/app.ini
 
 systemctl daemon-reload
 
--- roles/sourcecontrol/gogs/install.sh
+++ roles/sourcecontrol/gogs/install.sh
@@ -1,5 +1,5 @@
 docker pull gogs/gogs:0.11.29
 
-mkdir -p /var/gogs
+mkdir -p /var/local/gogs
 
-docker create --name=gogs -p 10022:22 -p 10080:3000 -v /var/gogs:/data gogs/gogs:0.11.29
+docker create --name=gogs -p 10022:22 -p 10080:3000 -v /var/local/gogs:/data gogs/gogs:0.11.29
