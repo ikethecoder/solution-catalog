4fd935a51238726652a385d2250720009e9202c6
Ike The Coder <ikethecoder@canzea.com>
2017-11-07 09:19:49 -0800

    canzea workaround

---
+++ roles/workarounds/canzea/config/profiled.sh
@@ -0,0 +1,11 @@
+export VAULT_CLIENT_CERT=/var/local/consul/ssl/vault.cert
+export VAULT_CLIENT_KEY=/var/local/consul/ssl/vault.key
+export VAULT_CACERT=/var/local/consul/ssl/ca.cert
+export VAULT_ADDR=https://vault.service.dc1.consul:8200
+export VAULT_URL=https://vault.service.dc1.consul:8200
+
+export CONSUL_CLIENT_CERT=/var/local/consul/ssl/consul.cert
+export CONSUL_CLIENT_KEY=/var/local/consul/ssl/consul.key
+export CONSUL_CACERT=/var/local/consul/ssl/ca.cert
+export CONSUL_ADDR=https://consul.service.dc1.consul:8080
+export CONSUL_URL=https://consul.service.dc1.consul:8080
--- roles/workarounds/canzea/install.sh
+++ roles/workarounds/canzea/install.sh
@@ -3,3 +3,5 @@ mkdir -p /root/.ecosystem-catalog
 
 yes | cp roles/workarounds/canzea/config/env.json /root/.ecosystem-catalog/env.json
 yes | cp roles/workarounds/canzea/config/config.json /root/.ecosystem-catalog/config.json
+
+yes | cp roles/workarounds/canzea/config/profiled.sh /etc/profile.d/hashicorp.sh
