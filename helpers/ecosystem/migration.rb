611c6f632b52b570690133dd9bf8af451cf00b80
Ike The Coder <ikethecoder@canzea.com>
2017-11-07 11:45:33 -0800

    migration script

---
+++ helpers/ecosystem/migration.rb
@@ -0,0 +1,25 @@
+
+require 'git'
+require 'json'
+require 'fileutils'
+require 'trace-runner'
+require 'commands/push-config'
+
+parameters = JSON.parse(ARGV[0])
+
+sourcePath = parameters['sourcePath']
+
+pc = PushConfig.new
+
+pc.cp sourcePath, "."
+
+ssh_config = %{
+    host es5ab5.canzea.cc
+     User root
+     Port 10022
+     IdentityFile ./sc/ecosystems/instances/terraform/.es/id_rsa_root_ecosystem
+}
+
+pc.write "ssh/config", ssh_config
+
+pc.commit "Migrated to ecosystems repository."
