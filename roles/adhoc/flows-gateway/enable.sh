12c4e6c220ce73f2fba971ef319185523c9ec917
Ike The Coder <ikethecoder@canzea.com>
2017-11-06 21:01:31 -0800

    flows-gateway upgrade

--- roles/adhoc/flows-gateway/enable.sh
+++ roles/adhoc/flows-gateway/enable.sh
@@ -1,11 +1,11 @@
 
-mkdir -p /home/pm2user/ssl
+mkdir -p /var/local/flows-gateway/ssl
 
-(cd /home/pm2user/ssl && openssl req -newkey rsa:1024 -nodes -out nodered.csr -keyout nodered.key -subj "/C=US/ST=Denial/L=Springfield/O=Canzea/CN=67.205.154.199")
+(cd /var/local/flows-gateway/ssl && openssl req -newkey rsa:1024 -nodes -out nodered.csr -keyout nodered.key -subj "/C=US/ST=Denial/L=Springfield/O=Canzea/CN=67.205.154.199")
 
-(cd /etc/consul.d/ssl/CA && openssl ca -batch -config myca.conf -notext -in /home/pm2user/ssl/nodered.csr -out /home/pm2user/ssl/nodered.cert)
+(cd /var/local/consul/ssl/CA && openssl ca -batch -config myca.conf -notext -in /var/local/flows-gateway/ssl/nodered.csr -out /var/local/flows-gateway/ssl/nodered.cert)
 
-cp /etc/consul.d/ssl/CA/ca.cert /home/pm2user/ssl/.
+cp /var/local/consul/ssl/CA/ca.cert /var/local/flows-gateway/ssl/.
 
 
 
@@ -15,3 +15,4 @@ yes | cp -f roles/adhoc/flows-gateway/config/flows-gateway.service /etc/systemd/
 systemctl daemon-reload
 
 systemctl restart flows-gateway
+
--- roles/adhoc/flows-gateway/install.sh
+++ roles/adhoc/flows-gateway/install.sh
@@ -1,4 +1,5 @@
 
-docker create --name flows-gateway -p 8000:8000 canzea/flows-gateway
+# docker create --name flows-gateway -p 8000:8000 -v /home/pm2user/ssl:/home/pm2user/ssl canzea/flows-gateway:0.1.1
 
 
+docker create --name flows-gateway -p 8000:8000 -v /var/local/flows-gateway/ssl:/home/pm2user/ssl -e VAULT_TOKEN -v /var/local/consul/ssl:/etc/vault/ssl canzea/flows-gateway:0.1.10
