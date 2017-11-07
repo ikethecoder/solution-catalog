039d7d40ab90672504f2e1fd3f37de3152c1d1b7
Ike The Coder <ikethecoder@canzea.com>
2017-11-07 14:50:31 -0800

    archiva first login

--- helpers/archiva/first-login.rb
+++ helpers/archiva/first-login.rb
@@ -7,6 +7,10 @@ driver = Selenium::WebDriver.for :remote, url: ENV['PHANTOMJS_URL']
 target_size = Selenium::WebDriver::Dimension.new(1024, 768)
 driver.manage.window.size = target_size
 
+user = ENV['SERVICE_ARCHIVA_ADMIN_CREDENTIALS_USER_NAME']
+pass = ENV['SERVICE_ARCHIVA_ADMIN_CREDENTIALS_PASSWORD']
+email = ENV['SERVICE_ARCHIVA_ADMIN_CREDENTIALS_EMAIL']
+
 driver.navigate.to "#{ENV["ARCHIVA_URL"]}"
 
 sleep 5
@@ -15,10 +19,10 @@ driver.find_element(:id, "create-admin-link-a").click
 
 sleep 5
 
-driver.find_element(:id, "password").send_keys "admin1admin1"
-driver.find_element(:id, "confirmPassword").send_keys "admin1admin1"
+driver.find_element(:id, "password").send_keys pass
+driver.find_element(:id, "confirmPassword").send_keys pass
 
-driver.find_element(:id, "email").send_keys "aidan.cope@gmail.com"
+driver.find_element(:id, "email").send_keys email
 
 driver.find_element(:id, "validated").click
 
