e7f1742150454c51deecc6366122357e006ff397
Ike The Coder <ikethecoder@canzea.com>
2017-11-07 17:36:32 -0800

    gocd-server make sure 8154 is open so go-agents can communicate.

--- roles/deploy/gocd-docker/install.sh
+++ roles/deploy/gocd-docker/install.sh
@@ -2,7 +2,7 @@
 
 docker build --tag gocd_local roles/deploy/gocd-docker/.
 
-docker create --name gocd -p 8153:8153 -p 8887:8887 -v /var/local/consul/ssl:/etc/consul.d/ssl gocd_local
+docker create --name gocd -p 8154:8154 -p 8153:8153 -p 8887:8887 -v /var/local/consul/ssl:/etc/consul.d/ssl gocd_local
 
 canzea --config_git_commit --template=roles/deploy/gocd-docker/config/gocd.service /etc/systemd/system/multi-user.target.wants/gocd.service
 
