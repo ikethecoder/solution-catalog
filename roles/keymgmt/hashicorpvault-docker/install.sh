94d664c992a5800a93aa0c0745a8871ecfd8e390
Ike The Coder <ikethecoder@canzea.com>
2017-11-09 07:46:44 -0800

    vault docker install fix

--- roles/keymgmt/hashicorpvault-docker/install.sh
+++ roles/keymgmt/hashicorpvault-docker/install.sh
@@ -1,6 +1,6 @@
 
 docker build --tag vault roles/keymgmt/hashicorpvault-docker
 
-(export HOST_IP=`curl -s http://169.254.169.254/metadata/v1/interfaces/private/0/ipv4/address` && docker create -d --dns=$HOST_IP --net=host --name vault -p 8200 -v /var/local/consul/ssl:/etc/consul.d/ssl --cap-add=IPC_LOCK vault server)
+(export HOST_IP=`curl -s http://169.254.169.254/metadata/v1/interfaces/private/0/ipv4/address` && docker create --dns=$HOST_IP --net=host --name vault -p 8200 -v /var/local/consul/ssl:/etc/consul.d/ssl --cap-add=IPC_LOCK vault server)
 
 (export HOST_IP=`curl -s http://169.254.169.254/metadata/v1/interfaces/private/0/ipv4/address` && echo "$HOST_IP vault.service.dc1.consul" >> /etc/hosts )
