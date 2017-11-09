aa9d77e31ade7eea787a7fee4f547ee9450e86b2
Ike The Coder <ikethecoder@canzea.com>
2017-11-09 07:23:57 -0800

    Upgrade install for docker to docker-ce

--- roles/container/docker/install.sh
+++ roles/container/docker/install.sh
@@ -2,7 +2,14 @@
 
 # yes | cp -f /root/ike-environments/environment/production/prod-1/Files/etc/yum.repos.d/docker.repo /etc/yum.repos.d/.
 
-yum -y install docker
+# Installation for adding the repo
+yum install -y yum-utils device-mapper-persistent-data lvm2
+
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+
+yum -y install docker-ce
 
 # Need to start during install phase as there may be "docker create" commands used
 sudo service docker start
+
+
