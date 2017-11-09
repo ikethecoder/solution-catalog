f67dc4b0dbf820b3a4f1e987444fb8b408af0f4c
Ike The Coder <ikethecoder@canzea.com>
2017-11-09 08:54:39 -0800

    Remove some puts that was showing private information

--- helpers/nodered/flows.rb
+++ helpers/nodered/flows.rb
@@ -9,7 +9,6 @@ file = parameters['file']
 uri = URI(ENV['NODERED_URL'] + '/admin/auth/token')
 
 res = Net::HTTP.post_form(uri , 'client_id' => 'node-red-admin', 'grant_type' => 'password', 'scope' => '*', 'username' => 'admin', 'password' => 'password')
-puts res.body
 
 data = JSON.parse(res.body)
 
