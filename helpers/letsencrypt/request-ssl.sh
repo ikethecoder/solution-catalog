40e8a3979385718d84a9df53851e36dee1cd5f91
Ike The Coder <ikethecoder@canzea.com>
2017-11-09 09:18:16 -0800

    request-ssl letsencrypt

---
+++ helpers/letsencrypt/request-ssl.sh
@@ -0,0 +1,19 @@
+while [[ $# -gt 1 ]]
+do
+  key="$1"
+
+  case $key in
+    --email)
+    export EMAIL="$2"
+    shift
+    ;;
+    --domain)
+    export DOMAIN="$2"
+    shift
+    ;;
+  esac
+
+  shift
+done
+
+docker exec nginx certbot certonly -q --standalone --email $EMAIL --text --agree-tos --rsa-key-size 4096 -d $DOMAIN
