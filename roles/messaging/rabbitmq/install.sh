155102f915f77467103d7fc13f5aa785afa161b9
Ike The Coder <ikethecoder@canzea.com>
2017-11-15 12:31:49 -0800

    Fix so that yum can find erlang

--- roles/messaging/rabbitmq/install.sh
+++ roles/messaging/rabbitmq/install.sh
@@ -1,5 +1,14 @@
 
+# Install ERLANG FIRST
+yum -y install wget
 
+curl -L -s -O https://packages.erlang-solutions.com/erlang-solutions-1.0-1.noarch.rpm
+rpm -Uvh erlang-solutions-1.0-1.noarch.rpm
+rm -f erlang-solutions-1.0-1.noarch.rpm
+
+yum -y install epel-release
+
+# Install
 yum -y install erlang
 
 curl -L -s -O http://www.rabbitmq.com/releases/rabbitmq-server/v3.6.0/rabbitmq-server-3.6.0-1.noarch.rpm
