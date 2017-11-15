34c0b142481bedac719c43d57a13dc199bc82f5d
Ike The Coder <ikethecoder@canzea.com>
2017-11-15 11:50:26 -0800

    Default filebeat to use elasticsearch direct, not logstash

--- roles/monitoring/filebeat/configure.sh
+++ roles/monitoring/filebeat/configure.sh
@@ -1,7 +1,7 @@
 
 
 # MOVE OVER THE CERTIFICATE TO EACH CLIENT SERVER
-canzea --raw --util=get-key-value certs/logstash | base64 --decode > /etc/pki/tls/certs/logstash-forwarder.crt
+# canzea --raw --util=get-key-value certs/logstash | base64 --decode > /etc/pki/tls/certs/logstash-forwarder.crt
 
 canzea --config_git_commit --template=roles/monitoring/filebeat/config/filebeat.yml /etc/filebeat/filebeat.yml
 
