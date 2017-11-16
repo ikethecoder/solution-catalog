d281773f313dfb624ab9f45495b195f42a863e92
Ike The Coder <ikethecoder@canzea.com>
2017-11-16 14:33:59 -0800

    digitalocean

--- helpers/users/configure-go.sh
+++ helpers/users/configure-go.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 mkdir /var/go/ssl
-cp /etc/consul.d/ssl/ca.cert /var/go/ssl/ca.cert
-cp /etc/consul.d/ssl/consul.cert /var/go/ssl/consul.cert
-cp /etc/consul.d/ssl/consul.key /var/go/ssl/consul.key
-cp /etc/consul.d/ssl/vault.cert /var/go/ssl/vault.cert
-cp /etc/consul.d/ssl/vault.key /var/go/ssl/vault.key
+cp /var/local/consul/ssl/ca.cert /var/go/ssl/ca.cert
+cp /var/local/consul/ssl/consul.cert /var/go/ssl/consul.cert
+cp /var/local/consul/ssl/consul.key /var/go/ssl/consul.key
+cp /var/local/consul/ssl/vault.cert /var/go/ssl/vault.cert
+cp /var/local/consul/ssl/vault.key /var/go/ssl/vault.key
 
 chown -R go:go /var/go/ssl
 
