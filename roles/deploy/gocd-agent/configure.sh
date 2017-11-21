cb7c44324106907d198e0f64271d95cb9bd8250d
Ike The Coder <ikethecoder@canzea.com>
2017-11-21 08:34:43 -0800

    Added docker-run to the go-agent install

--- roles/deploy/gocd-agent/configure.sh
+++ roles/deploy/gocd-agent/configure.sh
@@ -7,6 +7,10 @@ yes | cp -f roles/deploy/gocd-agent/config/register_service.sh /opt/canzea-utils
 
 chmod +x /opt/canzea-utils/register_service.sh
 
+yes | cp -f roles/deploy/gocd-agent/config/docker-run.sh /usr/bin/docker-run
+
+chmod +x /usr/bin/docker-run
+
 (su - go -c "echo -e 'y\n'|ssh-keygen -t rsa -f ~/.ssh/id_rsa -P ''")
 
 
