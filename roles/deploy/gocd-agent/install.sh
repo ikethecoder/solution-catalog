099ea82608992ec2193eec1dcffe3d7ea6b62e04
Ike The Coder <ikethecoder@canzea.com>
2017-11-21 08:37:06 -0800

    Create user go with a specific UID and GID so that the workers can have the correct access.

--- roles/deploy/gocd-agent/install.sh
+++ roles/deploy/gocd-agent/install.sh
@@ -5,6 +5,10 @@
 
 mkdir -p /var/go
 
+groupadd -g 1005 go
+
+useradd -u 1005 -g 1005 go
+
 curl -L -s -O https://download.gocd.io/binaries/17.3.0-4704/rpm/go-agent-17.3.0-4704.noarch.rpm
 
 yum -y localinstall go-agent-17.3.0-4704.noarch.rpm
