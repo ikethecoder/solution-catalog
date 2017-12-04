ed82d08cc63819f2eab4cb90978d633f554a5dd1
Ike The Coder <ikethecoder@canzea.com>
2017-12-04 14:20:44 -0800

    Script to upgrade the flows-gateway

---
+++ roles/adhoc/flows-gateway/upgrade.sh
@@ -0,0 +1,15 @@
+
+
+docker rm -f flows-gateway
+
+docker create --name flows-gateway -p 8000:8000 -v /var/local/flows-gateway/ssl:/home/pm2user/ssl -e VAULT_TOKEN -v /var/local/consul/ssl:/etc/vault/ssl canzea/flows-gateway:0.1.18
+
+systemctl start flows-gateway
+
+cp .ecosystem-catalog/catalog/blueprints/neat-and-tidy/flows/flows-e52e138766dcfa7715b7f582a00d8f0d.zip .
+
+mkdir -p temp && (cd temp && unzip ../flows-*.zip)
+
+
+(cd temp && canzea --lifecycle=wire --solution=nodered --action=flows --args='{"file":"nr-tab-id-global.flow"}')
+(cd temp && canzea --lifecycle=wire --solution=nodered --action=flows --args='{"file":"nr-tab-*.flow"}')
