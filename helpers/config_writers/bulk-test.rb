5cc784583edb9a182fafa9da5431db408e16a7be
Ike The Coder <ikethecoder@canzea.com>
2017-12-05 09:46:27 -0800

    Run config_writer but without commit

---
+++ helpers/config_writers/bulk-test.rb
@@ -0,0 +1,31 @@
+require 'json'
+require 'commands/push-config'
+require 'template-runner'
+require 'trace-runner'
+
+pc = PushConfig.new
+
+puts "Git Repository #{ENV['ECOSYSTEM_CONFIG_GIT']}"
+puts "Ecosystem #{ENV['ECOSYSTEM']}"
+puts "Hostname #{ENV['HOSTNAME']}"
+
+# Loop through the resources file and generate all the relevant content
+
+json = JSON.parse(ARGV[0])
+
+n = RunnerWorker.new(false)
+
+json['resources'].each do | resource |
+    rtype = resource.keys[0]
+
+    resource[rtype].keys.each do | rid |
+        puts rtype
+        puts "   #{rid}"
+
+
+        args = { rid => resource[rtype][rid] }
+
+        n.run "ruby #{ENV['CATALOG_LOCATION']}/helpers/config_writers/#{rtype}.rb '#{args.to_json}' PLUS", 0, 0
+    end
+
+end
