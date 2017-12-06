5d9b9040cd0fb385ab38fc7fbc0bd2afae08a8e7
Ike The Coder <ikethecoder@canzea.com>
2017-12-05 16:18:17 -0800

    Updates to IDE

--- roles/adhoc/ide/install.sh
+++ roles/adhoc/ide/install.sh
@@ -1,4 +1,7 @@
 
 git clone https://gitlab.com/canzea/ecosystem-catalog.git
 
-export WORKDIR=`pwd`/ecosystem-catalog && docker create -p 8000:8000 -v /root/client.js:/noide/config/client.js -v /root/server.json:/noide/config/server.json -v /var/local/consul/ssl:/etc/node/ssl -v $WORKDIR:/working --name ide canzea/ide:0.1.1
+
+(cd $CATALOG_LOCATION/roles/adhoc/ide && docker build --tag local_ide .)
+
+export WORKDIR=`pwd`/ecosystem-catalog && docker create -p 8000:8000 -v /var/local/consul/ssl:/etc/node/ssl -v $WORKDIR:/working --name ide local_ide
