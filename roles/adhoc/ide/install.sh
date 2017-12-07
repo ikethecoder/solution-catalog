975798dd8051b9fa29fccf77a9c692d091e159e8
Ike The Coder <ikethecoder@canzea.com>
2017-12-06 17:56:17 -0800

    Fix issue with IDE install

--- roles/adhoc/ide/install.sh
+++ roles/adhoc/ide/install.sh
@@ -2,6 +2,6 @@
 git clone https://gitlab.com/canzea/ecosystem-catalog.git
 
 
-(cd $CATALOG_LOCATION/roles/adhoc/ide && docker build --tag local_ide .)
+(cd roles/adhoc/ide && docker build --tag local_ide .)
 
 export WORKDIR=`pwd`/ecosystem-catalog && docker create -p 8000:8000 -v /var/local/consul/ssl:/etc/node/ssl -v $WORKDIR:/working --name ide local_ide
