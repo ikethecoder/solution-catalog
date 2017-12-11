acfedbfc5fcec7a4d808bf3148c18e08fa459799
Ike The Coder <ikethecoder@canzea.com>
2017-12-11 12:29:35 -0800

    Updates to IDE

--- roles/adhoc/ide/upgrade.sh
+++ roles/adhoc/ide/upgrade.sh
@@ -1,1 +1,7 @@
 
+docker rm -f ide
+export WORKDIR=`pwd`/ecosystem-catalog && docker create -p 8000:8000 -v /var/local/consul/ssl:/etc/node/ssl -v $WORKDIR:/working --name ide local_ide
+
+systemctl restart ide
+
+docker logs ide
