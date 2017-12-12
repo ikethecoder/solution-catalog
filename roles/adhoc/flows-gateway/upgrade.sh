e71fbd3f7f6372843a5bb3423132a19be1204924
Ike The Coder <ikethecoder@canzea.com>
2017-12-12 14:39:17 -0800

    Updated flows-gateway version

--- roles/adhoc/flows-gateway/upgrade.sh
+++ roles/adhoc/flows-gateway/upgrade.sh
@@ -2,7 +2,7 @@
 
 docker rm -f flows-gateway
 
-docker create --name flows-gateway -p 8000:8000 -v /var/local/flows-gateway/ssl:/home/pm2user/ssl -e VAULT_TOKEN -v /var/local/consul/ssl:/etc/vault/ssl canzea/flows-gateway:0.1.18
+docker create --name flows-gateway -p 8000:8000 -v /var/local/flows-gateway/ssl:/home/pm2user/ssl -e VAULT_TOKEN -v /var/local/consul/ssl:/etc/vault/ssl canzea/flows-gateway:0.1.19
 
 systemctl start flows-gateway
 
