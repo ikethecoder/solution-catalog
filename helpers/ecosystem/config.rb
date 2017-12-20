2abfff42e025c162cb4cec21c2bcf51749fbba86
Ike The Coder <ikethecoder@canzea.com>
2017-12-20 14:18:41 -0800

    Updated helpers for writer and config

--- helpers/ecosystem/config.rb
+++ helpers/ecosystem/config.rb
@@ -4,23 +4,24 @@ require 'fileutils'
 require 'trace-runner'
 require 'commands/push-config'
 
-pc = PushConfig.new "config"
-
 # Config --args='{"src":"abc","dest"}'
 
 params = JSON.parse(ARGV[0])
 
+pc = PushConfig.new "instances/#{params['instanceId']}/"
+
 source = params['source']
 target = File.realpath(params['target'])
+instanceId = params['instanceId']
+solution = params['solution']
 
 if File.exists? target
-    pc.cp target target
+    # pc.cp target, target
 end
 
-pc.cp source target
+pc.cp source, "config/#{solution}#{target}"
+puts "Writing to: config/#{solution}#{target}"
 
-FileUtils.cp source target
+FileUtils.cp source, target
 
 pc.commit "Tracking #{target}"
-
-
