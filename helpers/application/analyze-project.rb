6f8cd1763c6d2fb89f4d4eb0292e4c310ee1dd52
Ike The Coder <ikethecoder@canzea.com>
2017-12-29 15:44:01 -0800

    Made sure analzye always returns a good response.

--- helpers/application/analyze-project.rb
+++ helpers/application/analyze-project.rb
@@ -13,37 +13,46 @@ require_relative 'types/ruby/ruby-project'
 
 parameters = JSON.parse(ARGV[0])
 
-url = parameters['sourceRepo']
-branch = parameters['branch']
-name = parameters['name']
+begin
+    url = parameters['sourceRepo']
+    branch = parameters['branch']
+    name = parameters['name']
 
-if (parameters.has_key? 'credential_resource')
-    credentialResource = parameters['credential_resource']
-    creds = Registry.new.getSecret credentialResource
-    File.write("#{Dir.home}/.git-credentials", "https://#{creds['username']}:#{creds['password']}@gitlab.com")
-end
+    if (parameters.has_key? 'credential_resource')
+        credentialResource = parameters['credential_resource']
+        creds = Registry.new.getSecret credentialResource
+        File.write("#{Dir.home}/.git-credentials", "https://#{creds['username']}:#{creds['password']}@gitlab.com")
+    end
 
-if (File.exists? name)
-    FileUtils.remove_dir(name)
-end
+    if (File.exists? name)
+        FileUtils.remove_dir(name)
+    end
 
-# Need to incorporate the credentials if they are passed in
+    # Need to incorporate the credentials if they are passed in
 
-if (parameters.has_key? 'branch')
-    g = Git.clone(url, name, :branch => branch, :path => '.')
-else
-    g = Git.clone(url, name, :path => '.')
-end
+    if (parameters.has_key? 'branch')
+        g = Git.clone(url, name, :branch => branch, :path => '.')
+    else
+        g = Git.clone(url, name, :path => '.')
+    end
 
-branch = g.current_branch
-commit = g.log[0]
+    branch = g.current_branch
+    commit = g.log[0]
 
-if (File.exists? "#{name}/pom.xml")
-    puts JavaMavenProject.new.createDetails branch, commit, name
-elsif (File.exists? "#{name}/package.json")
-    puts JavascriptProject.new.createDetails branch, commit, name
-elsif (File.exists? "#{name}/Rakefile")
-    puts RubyProject.new.createDetails name
-else
-    raise "Unable to identify project type"
+    if (File.exists? "#{name}/pom.xml")
+        puts JavaMavenProject.new.createDetails branch, commit, name
+    elsif (File.exists? "#{name}/package.json")
+        puts JavascriptProject.new.createDetails branch, commit, name
+    elsif (File.exists? "#{name}/Rakefile")
+        puts RubyProject.new.createDetails name
+    else
+        raise "Unable to identify project type"
+    end
+rescue Exception => e
+    resp = {
+        "code" => "-1",
+        "error" => e.message,
+        "detail" => e.to_s
+    }
+    puts JSON.generate(resp)
 end
