2e3be74e77f215ce0cd2ffafc832a178b5295361
Ike The Coder <ikethecoder@canzea.com>
2017-12-29 20:32:44 -0800

    Static hugo build

--- helpers/config_writers/pipelines/hugo-build.rb
+++ helpers/config_writers/pipelines/hugo-build.rb
@@ -31,49 +31,46 @@ class HugoBuild
 
         taskTemplateCanzea = getFragmentPath("task-canzea.json")
 
-        if (type == "js-npm")
-            stage = JSON.parse(t.process stageTemplate, {"name" => "Build"})
+        stage = JSON.parse(t.process stageTemplate, {"name" => "Build"})
 
-            job = JSON.parse(t.process jobTemplate, attributes)
-            stage['jobs'].push(job)
+        job = JSON.parse(t.process jobTemplate, attributes)
+        stage['jobs'].push(job)
 
-            taskTemplate1 = getFragmentPath("task-docker.json")
+        taskTemplate1 = getFragmentPath("task-docker.json")
 
-            task = JSON.parse(t.process taskTemplate1, {"project" => attributes['project'], "arguments" => ["canzea/canzea_cli", "npm", "config", "set", "jobs", "1"] })
-            job['tasks'].push (task)
+        task = JSON.parse(t.process taskTemplate1, {"project" => attributes['project'], "arguments" => ["canzea/canzea_cli", "npm", "config", "set", "jobs", "1"] })
+        job['tasks'].push (task)
 
-            task = JSON.parse(t.process taskTemplate1, {"project" => attributes['project'], "arguments" => ["canzea/canzea_cli", "npm", "install"] })
-            job['tasks'].push (task)
+        task = JSON.parse(t.process taskTemplate1, {"project" => attributes['project'], "arguments" => ["canzea/canzea_cli", "npm", "install"] })
+        job['tasks'].push (task)
 
-            task = JSON.parse(t.process taskTemplate1, {"project" => attributes['project'], "arguments" => ["canzea/canzea_cli", "npm", "run", "build"] })
-            job['tasks'].push (task)
+        task = JSON.parse(t.process taskTemplate1, {"project" => attributes['project'], "arguments" => ["canzea/canzea_cli", "npm", "run", "build"] })
+        job['tasks'].push (task)
 
-            root['pipeline']['stages'].push (stage)
+        root['pipeline']['stages'].push (stage)
 
 
-            stage = JSON.parse(t.process stageTemplate, {"name" => "Registry"})
+        stage = JSON.parse(t.process stageTemplate, {"name" => "Registry"})
 
-            job = JSON.parse(t.process jobTemplate, attributes)
-            stage['jobs'].push(job)
+        job = JSON.parse(t.process jobTemplate, attributes)
+        stage['jobs'].push(job)
 
-            params = { }
-            params = params.to_json.to_json
-            params = params.slice(1,params.length - 2)
+        params = { }
+        params = params.to_json.to_json
+        params = params.slice(1,params.length - 2)
 
-            task = JSON.parse(t.process taskTemplateCanzea, {"workingdir" => attributes['project'], "project" => attributes['project'], "solution" => "gocd", "action" => "prep-build", "parameters" => params })
-            job['tasks'].push (task)
+        task = JSON.parse(t.process taskTemplateCanzea, {"workingdir" => attributes['project'], "project" => attributes['project'], "solution" => "gocd", "action" => "prep-build", "parameters" => params })
+        job['tasks'].push (task)
 
-            taskTemplate = getFragmentPath("task-mvn-package.json")
-            task = JSON.parse(t.process taskTemplate, {"project" => attributes['project']})
-            job['tasks'].push (task)
+        taskTemplate = getFragmentPath("task-mvn-package.json")
+        task = JSON.parse(t.process taskTemplate, {"project" => attributes['project']})
+        job['tasks'].push (task)
 
-            taskTemplate = getFragmentPath("task-mvn-deploy.json")
-            task = JSON.parse(t.process taskTemplate, {"project" => attributes['project']})
-            job['tasks'].push (task)
+        taskTemplate = getFragmentPath("task-mvn-deploy.json")
+        task = JSON.parse(t.process taskTemplate, {"project" => attributes['project']})
+        job['tasks'].push (task)
 
-            root['pipeline']['stages'].push (stage)
-
-        end
+        root['pipeline']['stages'].push (stage)
 
         item = {
             "pipeline_pipeline" => {
