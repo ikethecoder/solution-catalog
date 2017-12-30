6552df41b2e01aa13091d89d079ca24693255913
Ike The Coder <ikethecoder@canzea.com>
2017-12-29 17:58:00 -0800

    Updated flows-gateway

--- roles/adhoc/flows-gateway/install.sh
+++ roles/adhoc/flows-gateway/install.sh
@@ -1,2 +1,4 @@
 
-docker create --name flows-gateway -p 8000:8000 -e ECOSYSTEM -v /var/local/flows-gateway/ssl:/home/pm2user/ssl -e VAULT_TOKEN -v /var/local/consul/ssl:/etc/vault/ssl canzea/flows-gateway:0.1.22
+docker build --tag local_gw .
+
+docker create --name flows-gateway -p 8000:8000 -e ECOSYSTEM -v /var/local/flows-gateway/flows.json:/home/pm2user/.node-red/flows.json -v /var/local/flows-gateway/ssl:/home/pm2user/ssl -e VAULT_TOKEN -v /var/local/consul/ssl:/etc/vault/ssl local_gw
