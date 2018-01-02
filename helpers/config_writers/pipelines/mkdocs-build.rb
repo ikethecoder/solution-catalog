5f20f99415a116e2ad0eb7c5b177977e1da1753c
Ike The Coder <ikethecoder@canzea.com>
2018-01-02 11:22:49 -0800

    Pipeline

--- helpers/config_writers/pipelines/mkdocs-build.rb
+++ helpers/config_writers/pipelines/mkdocs-build.rb
@@ -112,7 +112,7 @@ class MkdocsBuild
         taskTemplate1 = getFragmentPath("task-docker-cli.json")
         taskTemplate2 = getFragmentPath("task-docker.json")
 
-        task = JSON.parse(t.process taskTemplate2, {"project" => attributes['project'], "arguments" => ["-e", "GO_PIPELINE_LABEL", "canzea/canzea_cli", "template", "#{attributes['project']}", "#{attributes['project']}"] })
+        task = JSON.parse(t.process taskTemplate2, {"project" => attributes['project'], "arguments" => ["-e", "GO_PIPELINE_LABEL", "canzea/canzea_cli", "template", "mkdocs.yml", "mkdocs.yml"] })
         job['tasks'].push (task)
 
         task = JSON.parse(t.process taskTemplate1, {"workdir" => "es-catalog/ecosystems/#{ENV['ECOSYSTEM']}/components/#{attributes['project']}", "arguments" => ["build", "--tag", "#{project}-task", "."] })
