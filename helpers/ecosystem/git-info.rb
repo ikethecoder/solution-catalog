07368dc3f1337b94ebecbb4b0bdb41b2c6f6d340
Ike The Coder <ikethecoder@canzea.com>
2018-01-03 16:13:13 -0800

    Git info helper

--- helpers/ecosystem/git-info.rb
+++ helpers/ecosystem/git-info.rb
@@ -3,9 +3,15 @@ require 'git'
 
 parameters = JSON.parse(ARGV[0])
 
-g = Git.open(parameters['path'])
+g = Git.open('.')
 
 branch = g.current_branch
 commit = g.log[0]
 
-puts JSON.generate({"branch" => branch, "commitRevision" => commit, "date" => commit.date.strftime('%FT%T%:z'), "message" => commit.message, "author" => {"name" => commit.author.name, "email" => commit.author.email} })
+g.remote('origin').fetch
+
+stats = g.diff('origin/master', commit).stats
+
+origin = g.gcommit('origin/master').sha
+
+puts JSON.generate({"branch" => branch, "commitRevision" => commit, "date" => commit.date.strftime('%FT%T%:z'), "message" => commit.message, "author" => {"name" => commit.author.name, "email" => commit.author.email}, "stats":stats, "origin":origin })
