9ad5e7e8ffaa3c352c55795bdc7622f4c8ddc8a1
Ike The Coder <ikethecoder@canzea.com>
2018-01-07 07:05:20 -0800

    flows-gateway update

--- roles/adhoc/flows-gateway/install.sh
+++ roles/adhoc/flows-gateway/install.sh
@@ -1,4 +1,4 @@
 
-docker build --tag local_gw .
+(cd roles/adhoc/flows-gateway && docker build --tag local_gw .)
 
 docker create --name flows-gateway -p 8000:8000 -e ECOSYSTEM -v /var/local/flows-gateway/flows.json:/home/pm2user/.node-red/flows.json -v /var/local/flows-gateway/ssl:/home/pm2user/ssl -e VAULT_TOKEN -v /var/local/consul/ssl:/etc/vault/ssl local_gw
