e9fe87dadd0a1515a10544f80eef39db14072500
Ike The Coder <ikethecoder@canzea.com>
2018-01-08 10:00:37 -0800

    Remove secret registration from rocket.chat configuration

--- roles/collaboration/rocketchat/configure.sh
+++ roles/collaboration/rocketchat/configure.sh
@@ -1,4 +1,4 @@
 
 
-
+# This is handled now in the instructions for the ecosystem
 # canzea --lifecycle=wire --solution=vault --action=register-secret --args='{"key":"admin/rocketchat", "data":{"password":"admin1admin"}}'
