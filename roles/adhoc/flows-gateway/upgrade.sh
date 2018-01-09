0a1d80a18442f3c685050c341ff5fcec8b86dedc
Ike The Coder <ikethecoder@canzea.com>
2018-01-09 15:21:12 -0800

    flows-gateway role update

--- roles/adhoc/flows-gateway/upgrade.sh
+++ roles/adhoc/flows-gateway/upgrade.sh
@@ -17,3 +17,8 @@ mkdir -p temp && (cd temp && unzip ../flows-*.zip)
 
 (cd temp && canzea --lifecycle=wire --solution=nodered --action=flows --args='{"file":"nr-tab-id-global.flow"}')
 (cd temp && canzea --lifecycle=wire --solution=nodered --action=flows --args='{"file":"nr-tab-*.flow"}')
+
+
+
+docker rm -f flows-gateway
+docker create --name flows-gateway -p 8000:8000 -e ECOSYSTEM -v /var/local/flows-gateway/flows.json:/home/pm2user/.node-red/flows.json -v /var/local/flows-gateway/ssl:/home/pm2user/ssl -e VAULT_TOKEN -v /var/local/consul/ssl:/etc/vault/ssl local_gw
