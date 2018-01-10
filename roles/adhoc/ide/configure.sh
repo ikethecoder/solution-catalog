0ce8278172730f9af663ddaa1c77ce4885a57a65
Ike The Coder <ikethecoder@canzea.com>
2018-01-10 14:56:53 -0800

    Changes for role ide

--- roles/adhoc/ide/configure.sh
+++ roles/adhoc/ide/configure.sh
@@ -1,4 +1,8 @@
 
+canzea --raw --util=get-key-value certs/ide.cert > /var/local/consul/ssl/ide.cert
+
+canzea --raw --util=get-key-value certs/ide.key > /var/local/consul/ssl/ide.key
+
 yes | cp -f roles/adhoc/ide/config/server.json /root/.
 
 yes | cp -f roles/adhoc/ide/config/ide.service /etc/systemd/system/multi-user.target.wants/.
--- roles/adhoc/ide/install.sh
+++ roles/adhoc/ide/install.sh
@@ -1,4 +1,4 @@
 
 (cd roles/adhoc/ide && docker build --tag local_ide .)
 
-export WORKDIR=`pwd`/ecosystems && docker create -p 8000:8000 -v /var/local/consul/ssl:/etc/node/ssl -v $WORKDIR:/working --name ide local_ide
+export WORKDIR=`pwd`/.es-catalog/catalog && docker create -p 8000:8000 -v /var/local/consul/ssl:/etc/node/ssl -v $WORKDIR:/working --name ide local_ide
