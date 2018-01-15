18e32d1f3bb637d376674a0353d80af69096d7ed
Ike The Coder <ikethecoder@canzea.com>
2018-01-15 11:04:53 -0800

    Update letsencrypt ssl

--- helpers/letsencrypt/request-ssl-alt02.sh
+++ helpers/letsencrypt/request-ssl-alt02.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+
+set -e
+
 while [[ $# -gt 1 ]]
 do
   key="$1"
@@ -26,8 +30,8 @@ echo "CERT_NAME = $CERT_NAME"
 
 sleep 5
 
-cp /var/local/nginx/conf.d/p80-default.conf /var/local/nginx/conf.d/p80-default.conf.oem
-cp /var/local/nginx/conf.d/p80-default.conf /var/local/nginx/conf.d/p80-default.conf.bak
+yes | cp -f /var/local/nginx/conf.d/p80-default.conf /var/local/nginx/conf.d/p80-default.conf.oem
+yes | cp -f /var/local/nginx/conf.d/p80-default.conf /var/local/nginx/conf.d/p80-default.conf.bak
 docker exec nginx certbot --authenticator webroot --cert-name $CERT_NAME --webroot-path /usr/share/nginx/html --installer nginx --email aidan.cope+letsencrypt@gmail.com --text --agree-tos --rsa-key-size 4096 -d $DOMAINS
 yes | cp -f /var/local/nginx/conf.d/p80-default.conf.bak /var/local/nginx/conf.d/p80-default.conf
 
