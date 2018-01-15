7e8ed3837a6de9ed23fb8140ef8c06eb28d0a5a3
Ike The Coder <ikethecoder@canzea.com>
2018-01-15 11:32:27 -0800

    Update letsencrypt ssl to use the EMAIL and to do non-interactive

--- helpers/letsencrypt/request-ssl-alt02.sh
+++ helpers/letsencrypt/request-ssl-alt02.sh
@@ -32,7 +32,7 @@ sleep 5
 
 yes | cp -f /var/local/nginx/conf.d/p80-default.conf /var/local/nginx/conf.d/p80-default.conf.oem
 yes | cp -f /var/local/nginx/conf.d/p80-default.conf /var/local/nginx/conf.d/p80-default.conf.bak
-docker exec nginx certbot --authenticator webroot --cert-name $CERT_NAME --webroot-path /usr/share/nginx/html --installer nginx --email aidan.cope+letsencrypt@gmail.com --text --agree-tos --rsa-key-size 4096 -d $DOMAINS
+docker exec nginx certbot --authenticator webroot --cert-name $CERT_NAME --webroot-path /usr/share/nginx/html --installer nginx --non-interactive --email $EMAIL --text --agree-tos --rsa-key-size 4096 -d $DOMAINS
 yes | cp -f /var/local/nginx/conf.d/p80-default.conf.bak /var/local/nginx/conf.d/p80-default.conf
 
 systemctl start nginx
