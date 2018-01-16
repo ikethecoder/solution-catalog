3897377ab728d003a6f2bf3215eb25d36526b725
Ike The Coder <ikethecoder@canzea.com>
2018-01-16 13:17:12 -0800

    ide - Make sure the directory is created before the docker container is; so that the volumes are mounted properly.

--- roles/adhoc/ide/install.sh
+++ roles/adhoc/ide/install.sh
@@ -3,4 +3,6 @@ canzea --config_git_commit --template=roles/adhoc/ide/config/server.json roles/a
 
 (cd roles/adhoc/ide && docker build --tag local_ide .)
 
+mkdir -p `pwd`/.es-catalog/catalog
+
 export WORKDIR=`pwd`/.es-catalog/catalog && docker create -p 8000:8000 -v /var/local/consul/ssl:/etc/node/ssl -v $WORKDIR:/working --name ide local_ide
