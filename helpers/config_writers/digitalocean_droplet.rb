ef1483833eb72841a6e8c4faca3e6968d709ead3
Ike The Coder <ikethecoder@canzea.com>
2018-01-17 14:07:54 -0800

    Added HOSTNAME as a standard env variable - used in helpers (for example report-audit)

--- helpers/config_writers/digitalocean_droplet.rb
+++ helpers/config_writers/digitalocean_droplet.rb
@@ -87,7 +87,7 @@ template = %{
                 "export HOSTNAME=`curl -s http://169.254.169.254/metadata/v1/hostname`",
                 "export PUBLIC_IPV4=`curl -s http://169.254.169.254/metadata/v1/interfaces/public/0/ipv4/address`",
                 "export PRIVATE_IPV4=`curl -s http://169.254.169.254/metadata/v1/interfaces/private/0/ipv4/address`",
-                "echo 'export ECOSYSTEM={{ecosystem}} && export ES_DOMAIN={{fqdn}}' > /etc/profile.d/canzea.sh"
+                "echo 'export ECOSYSTEM={{ecosystem}} && export ES_DOMAIN={{fqdn}} && export HOSTNAME=`hostname`' > /etc/profile.d/canzea.sh"
             ]
         }
     }
