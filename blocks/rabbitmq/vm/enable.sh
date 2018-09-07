ae3d8549c28bcfd9434f1211ac4b68ba1c4a5f8d
ikethecoder <ikethecoder@canzea.com>
2018-09-07 08:52:20 -0700

    rabbitmq - initial block

---
+++ blocks/rabbitmq/vm/enable.sh
@@ -0,0 +1,9 @@
+
+chkconfig rabbitmq-server on
+
+sudo service rabbitmq-server start
+
+# Need rabbitmq running to do this
+rabbitmqctl add_user ike geekkb
+rabbitmqctl set_user_tags ike administrator
+rabbitmqctl set_permissions -p / ike ".*" ".*" ".*"
