5abd3e71203ebf406c0dc0bb1b193dd02d2884bb
ikethecoder <ikethecoder@canzea.com>
2018-09-07 13:39:55 -0700

    vault cli - upgraded to 0.11.1

--- roles/keymgmt/hashicorpvault-cli/install.sh
+++ roles/keymgmt/hashicorpvault-cli/install.sh
@@ -1,8 +1,8 @@
 
-curl -s -O https://releases.hashicorp.com/vault/0.7.0/vault_0.7.0_linux_amd64.zip
+curl -s -O https://releases.hashicorp.com/vault/0.11.1/vault_0.11.1_linux_amd64.zip
 
-unzip -q vault_0.7.0_linux_amd64.zip
+unzip -q vault_0.11.1_linux_amd64.zip
 
-cp vault /usr/local/bin/.
+yes | cp vault /usr/local/bin/.
 
-rm -f vault_0.7.0_linux_amd64.zip
+rm -f vault_0.11.1_linux_amd64.zip
