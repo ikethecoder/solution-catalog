90383f0390abea7bab9b148f071075019e349597
ikethecoder <ikethecoder@canzea.com>
2018-09-07 15:25:33 -0700

    Rocket.chat downgrade version to avoid setup wizard added in 0.65

--- blocks/rocketchat/docker/enable.sh
+++ blocks/rocketchat/docker/enable.sh
@@ -1,12 +1,8 @@
 
 
-yes | cp -f blocks/rocketchat/docker/config/rocketchat-db.service /etc/systemd/system/multi-user.target.wants/.
-
 yes | cp -f blocks/rocketchat/docker/config/rocketchat.service /etc/systemd/system/multi-user.target.wants/.
 
 systemctl daemon-reload
-
-systemctl restart rocketchat-db
 
 systemctl restart rocketchat
 
