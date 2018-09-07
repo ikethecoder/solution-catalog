4e52fbd7a0a8b443c989589be073cf69217820d6
ikethecoder <ikethecoder@canzea.com>
2018-09-07 08:31:12 -0700

    canzea-upgrade changed to support git config

--- roles/workarounds/canzea-upgrade/install.sh
+++ roles/workarounds/canzea-upgrade/install.sh
@@ -1,2 +1,14 @@
 
 yes | cp roles/workarounds/canzea-upgrade/config/config-upgrade.json /root/.ecosystem-catalog/config.json
+
+
+git config --global user.name "SaaS Express Agent"
+git config --global user.password "support@canzea.com"
+
+git config --global credential.helper store
+
+export URL=SECRET_SERVICE_GITEA_ESADMIN_CREDENTIALS_URL
+
+echo "https://esadmin:${SECRET_GITEA_ROOT_TOKEN}@${URL}" > /root/.git-credentials
+chmod 600 /root/.git-credentials
+
