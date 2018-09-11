033669313567a1445540ed8787b79d1761d16529
ikethecoder <ikethecoder@canzea.com>
2018-09-11 15:51:46 -0700

    gitea block - config

---
+++ blocks/gitea/first-config/first-config.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+canzea --lifecycle=wire \
+  --solution=ecosystem \
+  --action=config \
+  --args='{"source":"blocks/gitea/docker/config/app.ini","target":"/var/local/gitea/gitea/conf/app.ini","instanceId":"cd-reg-01","solution":"gitea"}'
+
+systemctl restart gitea
