a492495a63161bf80b8b439b21826df5a6df0656
ikethecoder <ikethecoder@canzea.com>
2018-09-11 08:36:19 -0700

    gocd block - updated to externalize config/logs/etc

---
+++ blocks/gocd/docker/config/custom.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+yes | cp /go-server/plugins/external/generic-oauth-authorization-plugin-1.0.0-0.jar /godata/plugins/external/.
--- blocks/gocd/docker/install.sh
+++ blocks/gocd/docker/install.sh
@@ -2,6 +2,6 @@
 
 docker build --tag gocd_local blocks/gocd/docker/.
 
-docker create --name gocd -p 8154:8154 -p 8153:8153 -p 8887:8887 -v /var/local/consul/ssl:/etc/consul.d/ssl gocd_local
+mkdir -p /var/local/gocd
 
-
+docker create --name gocd -p 8154:8154 -p 8153:8153 -p 8887:8887 -v /var/local/gocd/godata:/godata -v /var/local/gocd/home:/home/go -v /var/local/consul/ssl:/etc/consul.d/ssl gocd_local
