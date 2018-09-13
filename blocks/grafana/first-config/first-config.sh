0077ae753897efcaada31e5a3b712a3c36c655b7
ikethecoder <ikethecoder@canzea.com>
2018-09-13 15:24:18 -0700

    grafana block - include first-config

--- blocks/grafana/first-config/first-config.sh
+++ blocks/grafana/first-config/first-config.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+set -e
+
 canzea --lifecycle=wire \
   --solution=ecosystem \
   --action=config \
-  --args='{"track":true, "source":"blocks/grafana/vm/config/grafana.ini","target":"/etc/grafana/grafana.ini","instanceId":"mon-mon-01","solution":"grafana"}'
+  --args='{"track":true, "source":"{{CATALOG_LOCATION}}/blocks/grafana/vm/config/grafana.ini","target":"/etc/grafana/grafana.ini","instanceId":"mon-mon-01","solution":"grafana"}'
 
 systemctl restart grafana-server
