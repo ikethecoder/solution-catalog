5fa100a3be2dd8e874034830c298b40f6be53e6a
ikethecoder <ikethecoder@canzea.com>
2018-09-13 16:39:59 -0700

    rabbitmq block - vm updates but still not working

--- blocks/rabbitmq/vm/install.sh
+++ blocks/rabbitmq/vm/install.sh
@@ -2,22 +2,24 @@
 # Install ERLANG FIRST
 yum -y install wget
 
+yum update
+
 yum -y install epel-release
 
 curl -L -s -O https://packages.erlang-solutions.com/erlang-solutions-1.0-1.noarch.rpm
 rpm -Uvh erlang-solutions-1.0-1.noarch.rpm
 rm -f erlang-solutions-1.0-1.noarch.rpm
 
-
 # Install
 yum -y install erlang
 
-curl -L -s -O http://www.rabbitmq.com/releases/rabbitmq-server/v3.6.0/rabbitmq-server-3.6.0-1.noarch.rpm
+yum -y remove rabbitmq-server
+curl -L -s -O http://www.rabbitmq.com/releases/rabbitmq-server/v3.6.15/rabbitmq-server-3.6.15-1.el6.noarch.rpm
 
 rpm --import https://www.rabbitmq.com/rabbitmq-signing-key-public.asc
 
-yum install -y rabbitmq-server-3.6.0-1.noarch.rpm
+yum install -y rabbitmq-server-3.6.15-1.el6.noarch.rpm
 
 
-rm -f rabbitmq-server-3.6.0-1.noarch.rpm
+rm -f rabbitmq-server-3.6.15-1.el7.noarch.rpm
 
