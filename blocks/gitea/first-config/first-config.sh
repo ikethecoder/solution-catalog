490b73482901055353c3b1c2aeaa918ad8767ef1
ikethecoder <ikethecoder@canzea.com>
2018-09-13 08:11:11 -0700

    gitea block - first-config fixed defect with location of config

--- blocks/gitea/first-config/first-config.sh
+++ blocks/gitea/first-config/first-config.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-set +e
+set -e
 
 canzea --lifecycle=wire \
   --solution=ecosystem \
   --action=config \
-  --args='{"source":"${CATALOG_LOCATION}/blocks/gitea/docker/config/app.ini","target":"/var/local/gitea/gitea/conf/app.ini","instanceId":"cd-reg-01","solution":"gitea"}'
+  --args='{"source":"{{CATALOG_LOCATION}}/blocks/gitea/docker/config/app.ini","target":"/var/local/gitea/gitea/conf/app.ini","instanceId":"cd-reg-01","solution":"gitea"}'
 
 systemctl restart gitea
 
