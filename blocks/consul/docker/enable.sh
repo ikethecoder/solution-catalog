a1ca77fb0595757d7faba162c8794b636c28e109
ikethecoder <ikethecoder@canzea.com>
2018-09-13 08:51:56 -0700

    consul block - add 'components' to consul keyvalue to fix projects defect

--- blocks/consul/docker/enable.sh
+++ blocks/consul/docker/enable.sh
@@ -5,3 +5,5 @@ systemctl start consul
 
 
 (export PRIVATE_IPV4=`curl -s http://169.254.169.254/metadata/v1/interfaces/private/0/ipv4/address` && echo "nameserver $PRIVATE_IPV4" > /etc/resolv.conf )
+
+docker exec consul consul kv put components/
