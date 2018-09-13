95f1b065da9b937a3fcbdeab1250e2dc393dc80b
ikethecoder <ikethecoder@canzea.com>
2018-09-12 23:14:08 -0700

    canzea-upgrade - fix bug with accessing repo

--- roles/workarounds/canzea-upgrade/configure.sh
+++ roles/workarounds/canzea-upgrade/configure.sh
@@ -1,7 +1,7 @@
 
 
 git config --global user.name "SaaS Express Agent"
-git config --global user.password "support@canzea.com"
+git config --global user.email "saas_express_agent@canzea.com"
 
 git config --global credential.helper store
 
@@ -9,5 +9,7 @@ echo "http://esadmin:${GITEA_ROOT_TOKEN}@gitea.service.dc1.consul:11080" > /root
 chmod 600 /root/.git-credentials
 
 yes | cp -f roles/workarounds/canzea-upgrade/config/config-upgrade.json /root/.ecosystem-catalog/config.json
+
+canzea --util=add-env ECOSYSTEM_CONFIG_GIT "http://gitea.service.dc1.consul:11080/esadmin/ecosystems.git"
 
 canzea --reset
