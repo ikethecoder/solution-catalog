793667c1addd3376afe65967f13db4632853a83e
ikethecoder <ikethecoder@canzea.com>
2018-09-13 09:42:53 -0700

    rocketchat block - set-oauth use setValueSlow

--- blocks/rocketchat/first-set-oauth/first-set-oauth.sh
+++ blocks/rocketchat/first-set-oauth/first-set-oauth.sh
@@ -21,7 +21,7 @@ docker run --net=vlan1 --rm \
   -e SERVICE_ROCKETCHAT_ESADMIN_CREDENTIALS_PASSWORD \
   -e OAUTH_CLIENTS_ROCKETCHAT_OAUTH2_ROOT \
   -e OAUTH_CLIENTS_ROCKETCHAT_CLIENT_SECRET \
-  canzea/tester:0.1.3
+  canzea/tester:0.1.5
 
 docker rm -f selenium || true
 
