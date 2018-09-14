1f8a250a0a6dc2dec0eb4ee0f62bcd75cfb6bc9c
ikethecoder <ikethecoder@canzea.com>
2018-09-14 07:32:29 -0700

    gocd block - first-config for websocket plugin and git creds

--- blocks/gocd/first-config/first-config.sh
+++ blocks/gocd/first-config/first-config.sh
@@ -7,5 +7,15 @@ canzea --lifecycle=wire \
   --action=config \
   --args='{"source":"{{CATALOG_LOCATION}}/blocks/gocd/first-config/config/gocd-websocket-notifier.conf","target":"/var/local/gocd/home/gocd-websocket-notifier.conf","instanceId":"cd-reg-01","solution":"gocd"}'
 
+canzea --lifecycle=wire \
+  --solution=ecosystem \
+  --action=config \
+  --args='{"source":"{{CATALOG_LOCATION}}/blocks/gocd/first-config/config/.gitconfig.mustache","target":"/var/local/gocd/home/.gitconfig","instanceId":"cd-reg-01","solution":"gocd"}'
+
+canzea --lifecycle=wire \
+  --solution=ecosystem \
+  --action=config \
+  --args='{"source":"{{CATALOG_LOCATION}}/blocks/gocd/first-config/config/.git-credentials.mustache","target":"/var/local/gocd/home/.git-credentials","instanceId":"cd-reg-01","solution":"gocd"}'
+
 systemctl restart gocd-server
 
