0b8e10f7057ee2ea69d8ecbcee28bc4607698be8
ikethecoder <ikethecoder@canzea.com>
2018-10-10 09:27:10 -0700

    archiva block - ugprade first-login - missed archiva_url

--- blocks/archiva/first-login/first-login.sh
+++ blocks/archiva/first-login/first-login.sh
@@ -7,6 +7,7 @@ docker run --net=host --rm \
   -v $CATALOG_LOCATION/blocks/archiva/first-login/scripts:/tests \
   -e ENV=local \
   -e PHANTOMJS_URL \
+  -e ARCHIVA_URL \
   -e SERVICE_ARCHIVA_ESADMIN_CREDENTIALS_URL \
   -e SERVICE_ARCHIVA_ESADMIN_CREDENTIALS_EMAIL \
   -e SERVICE_ARCHIVA_ESADMIN_CREDENTIALS_USER_NAME \
