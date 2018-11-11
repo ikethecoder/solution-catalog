e603f623156f424281070bff51191fe30cdd8471
ikethecoder <ikethecoder@canzea.com>
2018-11-11 12:45:28 -0800

    workaround canzea enhancement to support passing branch

--- roles/workarounds/canzea/install.sh
+++ roles/workarounds/canzea/install.sh
@@ -2,7 +2,9 @@
 mkdir -p /root/.ecosystem-catalog
 
 yes | cp roles/workarounds/canzea/config/env.json /root/.ecosystem-catalog/env.json
-yes | cp roles/workarounds/canzea/config/config.json /root/.ecosystem-catalog/config.json
+# yes | cp roles/workarounds/canzea/config/config.json /root/.ecosystem-catalog/config.json
+
+canzea --config_git_commit --template=roles/workarounds/canzea/config/config.json /root/.ecosystem-catalog/config.json
 
 yes | cp roles/workarounds/canzea/config/profiled.sh /etc/profile.d/hashicorp.sh
 
