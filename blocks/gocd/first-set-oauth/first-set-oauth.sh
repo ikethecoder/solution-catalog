6f59e163f64bbc9fa521462934c33cee1478b89a
ikethecoder <ikethecoder@canzea.com>
2018-11-11 19:31:07 -0800

    gocd first-set-oauth

--- blocks/gocd/first-set-oauth/first-set-oauth.sh
+++ blocks/gocd/first-set-oauth/first-set-oauth.sh
@@ -15,15 +15,15 @@ docker run --net=vlan1 -d -p 4444:4444 --name selenium -v /dev/shm:/dev/shm sele
 sleep 5
 
 docker run --net=vlan1 --rm \
-  -v `pwd`:/screenshots \
-  -v `pwd`/blocks/gocd/first-set-oauth/scripts:/tests \
+  -v $CATALOG_LOCATION/:/screenshots \
+  -v $CATALOG_LOCATION/blocks/gocd/first-set-oauth/scripts:/tests \
   -e ENV=local \
   -e GOCD_ADDRESS \
   -e GOCD_PORT \
   -e SERVICE_GOCD_SP_GOCD_ADMIN_CREDENTIALS_PASSWORD \
   -e OAUTH_CLIENTS_GOCD_OAUTH2_ROOT_URL \
   -e OAUTH_CLIENTS_GOCD_CLIENT_SECRET \
-  canzea/tester:0.1.3
+  canzea/tester:0.1.13
 
 docker rm -f selenium || true
 
