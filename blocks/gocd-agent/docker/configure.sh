0b06083eb5bf782d348c5cc4a572f505e0668cc4
ikethecoder <ikethecoder@canzea.com>
2018-11-23 10:22:49 -0800

    gocd-agent block upgrades

--- blocks/gocd-agent/docker/configure.sh
+++ blocks/gocd-agent/docker/configure.sh
@@ -1,4 +1,4 @@
 
-docker create --name gocd-agent --net=vlan0 -e GO_SERVER_URL=$SERVICE_GOCD_SP_GOCD_ADMIN_CREDENTIALS_URL gocd/gocd-agent-alpine-3.5:v18.10.0
+docker rm -f gocd-agent || true
 
-
+docker create --name gocd-agent --net=vlan0 -v /var/local/gocd/home:/home/go -e GO_SERVER_URL=$SERVICE_GOCD_SP_GOCD_ADMIN_CREDENTIALS_URL gocd/gocd-agent-alpine-3.5:v18.10.0
--- blocks/gocd-agent/first-config/first-config.sh
+++ blocks/gocd-agent/first-config/first-config.sh
@@ -5,13 +5,13 @@ set -e
 canzea --lifecycle=wire \
   --solution=ecosystem \
   --action=config \
-  --args='{"source":"{{CATALOG_LOCATION}}/blocks/gocd-agent/first-config/config/.gitconfig.mustache","target":"/var/go/.gitconfig","instanceId":"{{HOSTNAME}}","solution":"gocd-agent"}'
+  --args='{"source":"{{CATALOG_LOCATION}}/blocks/gocd-agent/first-config/config/.gitconfig.mustache","target":"/var/local/gocd/home/.gitconfig","instanceId":"{{HOSTNAME}}","solution":"gocd-agent"}'
 
 canzea --lifecycle=wire \
   --solution=ecosystem \
   --action=config \
-  --args='{"source":"{{CATALOG_LOCATION}}/blocks/gocd-agent/first-config/config/.git-credentials.mustache","target":"/var/go/.git-credentials","instanceId":"{{HOSTNAME}}","solution":"gocd-agent"}'
+  --args='{"source":"{{CATALOG_LOCATION}}/blocks/gocd-agent/first-config/config/.git-credentials.mustache","target":"/var/local/gocd/home/.git-credentials","instanceId":"{{HOSTNAME}}","solution":"gocd-agent"}'
 
 chmod 644 /var/go/.git*
 
-systemctl stop go-agent && systemctl start go-agent
+systemctl stop gocd-agent && systemctl start gocd-agent
