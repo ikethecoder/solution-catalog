766a5f552b12c68ed37e451b3c86cb8a7b351ebb
ikethecoder <ikethecoder@canzea.com>
2018-11-22 23:24:44 -0800

    keycloak block enhancements for first-config

---
+++ blocks/keycloak/first-config/keycloak-setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -e
+
+export KEYCLOAK_HOME=`pwd`/keycloak
+export PATH=$PATH:$KEYCLOAK_HOME/bin
+
+keycloak/bin/kcadm.sh config credentials --server http://keycloak:8080/auth --realm master --user $KEYCLOAK_USER --client admin-cli --password $KEYCLOAK_PASSWORD
+
+keycloak/bin/kcadm.sh create realms -s realm=ecosystem -s enabled=true -o
+
+CID=$(kcadm.sh create clients -r ecosystem -s clientId=gateway -s enabled=true -s clientAuthenticatorType=client-secret -s secret=$KEYCLOAK_CLIENT_SECRET -s "redirectUris=[\"http://*\",\"https://*\"]" -i)
