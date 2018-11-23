4176b0358a2d34c4014e2540fd551d68e4acb8e9
ikethecoder <ikethecoder@canzea.com>
2018-11-23 10:34:21 -0800

    gocd-agent block upgrades to latest alpine

--- blocks/gocd-agent/docker/configure.sh
+++ blocks/gocd-agent/docker/configure.sh
@@ -1,4 +1,5 @@
 
 docker rm -f gocd-agent || true
 
-docker create --name gocd-agent --net=vlan0 -v /var/local/gocd/home:/home/go -e AGENT_AUTO_REGISTER_KEY=$HOSTNAME -e AGENT_AUTO_REGISTER_HOSTNAME=$HOSTNAME -e GO_SERVER_URL=$SERVICE_GOCD_SP_GOCD_ADMIN_CREDENTIALS_URL gocd/gocd-agent-alpine-3.5:v18.10.0
+echo "HOSTNAME=$HOSTNAME"
+docker create --name gocd-agent --net=vlan0 -v /var/local/gocd/home:/home/go -e AGENT_AUTO_REGISTER_KEY=$HOSTNAME -e AGENT_AUTO_REGISTER_HOSTNAME=$HOSTNAME -e GO_SERVER_URL=$SERVICE_GOCD_SP_GOCD_ADMIN_CREDENTIALS_URL gocd/gocd-agent-alpine-3.6:v18.10.0
