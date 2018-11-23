c379eb5efecfe486e2264599f44eca70cf932c2a
ikethecoder <ikethecoder@canzea.com>
2018-11-22 22:25:14 -0800

    block postgres and keycloak draft

--- blocks/keycloak/docker/configure.sh
+++ blocks/keycloak/docker/configure.sh
@@ -1,3 +1,3 @@
 
 
-docker run --net=vlan0 -v $CATALOG_LOCATION/blocks/keycloak/docker/config:/work postgres:9.6.9 psql postgresql://pguser:mysecretpassword@postgres -f /work/setup.psql
+docker run --net=vlan0 -v ${PWD}/blocks/keycloak/docker/config:/work postgres:9.6.9 psql postgresql://pguser:mysecretpassword@postgres -f /work/setup.psql
