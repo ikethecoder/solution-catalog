00b253e06b7f8ff589c6e1fa6a674978ee625c29
ikethecoder <ikethecoder@canzea.com>
2018-11-22 23:31:18 -0800

    keycloak block enhancements for first-config

--- blocks/keycloak/first-config/first-config.sh
+++ blocks/keycloak/first-config/first-config.sh
@@ -6,5 +6,5 @@ export KEYCLOAK_USER=kcadmin
 export KEYCLOAK_PASSWORD=kcadmin#1
 export KEYCLOAK_CLIENT_SECRET=11-11-11-11
 
-docker run --net=vlan0 -e CD_KEYCLOAK_URL -e KEYCLOAK_USER -e KEYCLOAK_PASSWORD -e KEYCLOAK_CLIENT_SECRET -v $CATALOG_LOCATION/blocks/keycloak/first-config:/work --entrypoint /bin/bash jboss/keycloak:4.1.0.Final -c /work/keycloak-setup.sh
+docker run --net=vlan0 -e CD-KEYCLOAK_URL -e KEYCLOAK_USER -e KEYCLOAK_PASSWORD -e KEYCLOAK_CLIENT_SECRET -v $CATALOG_LOCATION/blocks/keycloak/first-config:/work --entrypoint /bin/bash jboss/keycloak:4.1.0.Final -c /work/keycloak-setup.sh
 
