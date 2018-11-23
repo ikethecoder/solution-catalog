0a054df09c70c5d430ba35d15b8f1f315f822fba
ikethecoder <ikethecoder@canzea.com>
2018-11-23 01:19:11 -0800

    rocketchat block - set-site-url update

--- blocks/rocketchat/first-set-site-url/first-set-site-url.sh
+++ blocks/rocketchat/first-set-site-url/first-set-site-url.sh
@@ -24,7 +24,7 @@ docker run --net=vlan1 --rm \
   -e SERVICE_ROCKETCHAT_ESADMIN_CREDENTIALS_URL \
   -e OAUTH_CLIENTS_ROCKETCHAT_OAUTH2_ROOT \
   -e OAUTH_CLIENTS_ROCKETCHAT_CLIENT_SECRET \
-  canzea/tester:0.1.13
+  canzea/test-nightwatch-runner:latest
 
 docker rm -f selenium || true
 
