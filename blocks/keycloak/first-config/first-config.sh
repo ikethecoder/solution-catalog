a2a32cc11486f83763125b4099ca4e56e9290748
ikethecoder <ikethecoder@canzea.com>
2018-11-23 00:10:48 -0800

    keycloak block update first-config

--- blocks/keycloak/first-config/first-config.sh
+++ blocks/keycloak/first-config/first-config.sh
@@ -4,7 +4,7 @@ set -e
 
 export KEYCLOAK_USER=kcadmin
 export KEYCLOAK_PASSWORD=kcadmin#1
-export KEYCLOAK_CLIENT_SECRET=11-11-11-11
+export KEYCLOAK_CLIENT_SECRET=$OAUTH_CLIENTS_GITEA_CLIENT_SECRET
 
 docker run --net=vlan0 -e KEYCLOAK_URL=$SERVICE_KEYCLOAK_ESADMIN_CREDENTIALS_URL -e KEYCLOAK_USER -e KEYCLOAK_PASSWORD -e KEYCLOAK_CLIENT_SECRET -v $CATALOG_LOCATION/blocks/keycloak/first-config:/work --entrypoint /bin/bash jboss/keycloak:4.1.0.Final -c /work/keycloak-setup.sh
 
