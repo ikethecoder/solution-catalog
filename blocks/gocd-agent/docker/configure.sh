aaf4d31107071843e432042f49ad0fbc3f53f1ef
ikethecoder <ikethecoder@canzea.com>
2018-11-23 10:13:51 -0800

    block gocd-agent docker

--- blocks/gocd-agent/docker/configure.sh
+++ blocks/gocd-agent/docker/configure.sh
@@ -1,4 +1,4 @@
 
-docker create --name gocd-agent --net=vlan0 -e GO_SERVER_URL=$SERVICE_GOCD_SPADMIN_CREDENTIALS_URL gocd/gocd-agent-alpine-3.5:v18.10.0
+docker create --name gocd-agent --net=vlan0 -e GO_SERVER_URL=$SERVICE_GOCD_SP_GOCD_ADMIN_CREDENTIALS_URL gocd/gocd-agent-alpine-3.5:v18.10.0
 
 
