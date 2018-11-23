1bc2b9afeba02f9303ae9234737db16ae9ae53e2
ikethecoder <ikethecoder@canzea.com>
2018-11-23 11:30:18 -0800

    gocd-agent block allow DIND

--- blocks/gocd-agent/docker/configure.sh
+++ blocks/gocd-agent/docker/configure.sh
@@ -3,6 +3,8 @@ docker rm -f gocd-agent || true
 
 echo "HOSTNAME=$HOSTNAME"
 
-canzea --lifecycle=wire --solution=ecosystem --action=config --args='{"source":"{{CATALOG_LOCATION}}/blocks/gocd-agent/docker/config/docker-run.sh","target":"/var/local/gocd/home/docker-run.sh","instanceId":"{{HOSTNAME}}","solution":"gocd-agent"}'
+canzea --lifecycle=wire --solution=ecosystem --action=config --args='{"source":"{{CATALOG_LOCATION}}/blocks/gocd-agent/docker/config/docker-run.sh","target":"/var/local/gocd/home/docker-run","instanceId":"{{HOSTNAME}}","solution":"gocd-agent"}'
 
-docker create --name gocd-agent --net=vlan0 -h $HOSTNAME -v /var/local/gocd/home:/home/go -v /var/local/gocd/home:/usr/local/bin -e GO_SERVER_URL=https://${GOCD_ADDRESS}:8154/go gocd/gocd-agent-alpine-3.7:v18.7.0
+chmod +x /var/local/gocd/home/docker-run
+
+docker create --name gocd-agent --net=vlan0 --link dind:docker -h $HOSTNAME -v /var/local/gocd/home:/home/go -v /var/local/gocd/home:/usr/local/bin -e GO_SERVER_URL=https://${GOCD_ADDRESS}:8154/go gocd/gocd-agent-alpine-3.7:v18.7.0
