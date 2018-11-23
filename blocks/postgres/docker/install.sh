97eba7a29b36fe5c6c095010831210497958f7e1
ikethecoder <ikethecoder@canzea.com>
2018-11-22 21:42:26 -0800

    block postgres draft

--- blocks/postgres/docker/install.sh
+++ blocks/postgres/docker/install.sh
@@ -1,7 +1,3 @@
 
 
-docker create --name postgres \
-  -e POSTGRES_USER=pguser \
-  -e POSTGRES_PASSWORD=mysecretpassword \
-  -v /var/local/postgres/data:/var/lib/postgresql/data \
-  postgres:9.6.9
+docker create --name postgres -e POSTGRES_USER=pguser -e POSTGRES_PASSWORD=mysecretpassword -v /var/local/postgres/data:/var/lib/postgresql/data postgres:9.6.9
