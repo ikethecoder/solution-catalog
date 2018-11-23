e206bbf2ba81860905a765a9f5e1c35cc7c54d81
ikethecoder <ikethecoder@canzea.com>
2018-11-23 11:19:01 -0800

    gocd-agent block add runner

--- blocks/gocd-agent/docker/configure.sh
+++ blocks/gocd-agent/docker/configure.sh
@@ -3,4 +3,9 @@ docker rm -f gocd-agent || true
 
 echo "HOSTNAME=$HOSTNAME"
 
-docker create --name gocd-agent --net=vlan0 -h $HOSTNAME -v /var/local/gocd/home:/home/go -e GO_SERVER_URL=$SERVICE_GOCD_SP_GOCD_ADMIN_CREDENTIALS_URL gocd/gocd-agent-alpine-3.6:v18.10.0
+canzea --lifecycle=wire \
+  --solution=ecosystem \
+  --action=config \
+  --args='{"source":"{{CATALOG_LOCATION}}/blocks/gocd-agent/docker/config/docker-run.sh","target":"/var/local/gocd/home/docker-run.sh","instanceId":"{{HOSTNAME}}","solution":"gocd-agent"}'
+
+docker create --name gocd-agent --net=vlan0 -h $HOSTNAME -v /var/local/gocd/home:/home/go -e GO_SERVER_URL=https://${GOCD_ADDRESS}:8154 gocd/gocd-agent-alpine-3.7:v18.7.0
