3a255eb16fedf68e98418c06db211ddbb786f924
ikethecoder <ikethecoder@canzea.com>
2018-11-23 12:38:05 -0800

    gocd-agent set-registry-creds

--- blocks/gocd-agent/set-registry-creds/set-registry-creds.sh
+++ blocks/gocd-agent/set-registry-creds/set-registry-creds.sh
@@ -7,3 +7,4 @@ canzea --lifecycle=wire \
   --action=config \
   --args='{"source":"{{CATALOG_LOCATION}}/blocks/gocd-agent/set-registry-creds/config/settings.xml.tmpl","target":"/etc/maven/settings.xml","instanceId":"{{HOSTNAME}}","solution":"maven"}'
 
+
