c938d6c74eaf959b2b0004a52e6bf41dc154e332
ikethecoder <ikethecoder@canzea.com>
2018-11-23 03:04:57 -0800

    kibana update

--- blocks/kibana/docker/configure.sh
+++ blocks/kibana/docker/configure.sh
@@ -1,3 +1,3 @@
 
 
-docker create --name kibana --net=vlan0 -p 5601:5601 -e ELASTICSEARCH_URL docker.elastic.co/kibana/kibana:6.3.2
+docker create --name kibana --net=vlan0 -p 5601:5601 -e ELASTICSEARCH_URL -e XPACK_MONITORING_ENABLED=true docker.elastic.co/kibana/kibana:6.3.2
