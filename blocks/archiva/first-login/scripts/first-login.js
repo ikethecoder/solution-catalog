3892d96d7f4b38b733ab3feb431395b7ffe1ebbc
ikethecoder <ikethecoder@canzea.com>
2018-11-23 00:47:04 -0800

    archiva block - first-login update

--- blocks/archiva/first-login/scripts/first-login.js
+++ blocks/archiva/first-login/scripts/first-login.js
@@ -6,7 +6,7 @@ module.exports = {
       browser
         .url(browser.config.archiva.url)
         .waitForElementVisible('body', 1000)
-        .click("a[id='create-admin-link-a']")
+        .click("#create-admin-link-a")
         .waitForElementVisible("input[name='password']", 5000)
         .setValue("input[name='password']", process.env.SERVICE_ARCHIVA_ESADMIN_CREDENTIALS_PASSWORD)
         .setValue("input[name='confirmPassword']", process.env.SERVICE_ARCHIVA_ESADMIN_CREDENTIALS_PASSWORD)
