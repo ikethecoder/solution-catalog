536f2e23e47f49770f0f5e9bb748924913f40366
ikethecoder <ikethecoder@canzea.com>
2018-11-23 19:55:35 -0800

    flows-gateway revert back to 0.1.33

--- blocks/flows-gateway/upgrade-0.1.34/upgrade-0.1.34.sh
+++ blocks/flows-gateway/upgrade-0.1.34/upgrade-0.1.34.sh
@@ -12,6 +12,6 @@ docker create --name flows-gateway -p 8000:8000 \
     -e ADMIN_OAUTH2_CLIENT_SECRET=$OAUTH_CLIENTS_GITEA_CLIENT_SECRET \
     -e ADMIN_OAUTH2_ISSUER=$OAUTH_CLIENTS_GITEA_OIDC_DISCOVERY \
     -e ADMIN_OAUTH2_CALLBACK="https://$ES_DOMAIN/gwadmin/auth/strategy/callback" \
-    -e ECOSYSTEM -e ECOSYSTEM_LABEL="Ecosystem Gateway" -v /var/local/flows-gateway/flows.json:/home/pm2user/.node-red/flows.json -v /var/local/flows-gateway/ssl:/home/pm2user/ssl -e VAULT_TOKEN -v /var/local/consul/ssl:/etc/vault/ssl canzea/flows-gateway:0.1.34
+    -e ECOSYSTEM -e ECOSYSTEM_LABEL="Ecosystem Gateway" -v /var/local/flows-gateway/flows.json:/home/pm2user/.node-red/flows.json -v /var/local/flows-gateway/ssl:/home/pm2user/ssl -e VAULT_TOKEN -v /var/local/consul/ssl:/etc/vault/ssl canzea/flows-gateway:0.1.33
 
 systemctl restart flows-gateway
