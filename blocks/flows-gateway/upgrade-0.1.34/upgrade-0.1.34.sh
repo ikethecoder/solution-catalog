14d90bcada994e719c762cee9a657e29aaff6468
ikethecoder <ikethecoder@canzea.com>
2018-11-23 22:19:01 -0800

    flows-gateway upgrade

--- blocks/flows-gateway/upgrade-0.1.34/upgrade-0.1.34.sh
+++ blocks/flows-gateway/upgrade-0.1.34/upgrade-0.1.34.sh
@@ -3,7 +3,7 @@
 docker rm -f flows-gateway || true
 
 docker create --name flows-gateway -p 8000:8000 \
-    -e adminAuth=normal \
+    -e adminAuth=oauth2 \
     -e adminRoot="/gwadmin/" \
     -e ADMIN_OAUTH2_AUTHORIZE="$OAUTH_CLIENTS_GITEA_OIDC_ISSUER/protocol/openid-connect/auth" \
     -e ADMIN_OAUTH2_TOKEN="$OAUTH_CLIENTS_GITEA_OIDC_ISSUER/protocol/openid-connect/token" \
@@ -12,7 +12,7 @@ docker create --name flows-gateway -p 8000:8000 \
     -e ADMIN_OAUTH2_CLIENT_SECRET=$OAUTH_CLIENTS_GITEA_CLIENT_SECRET \
     -e ADMIN_OAUTH2_ISSUER=$OAUTH_CLIENTS_GITEA_OIDC_DISCOVERY \
     -e ADMIN_OAUTH2_CALLBACK="https://$ES_DOMAIN/gwadmin/auth/strategy/callback" \
-    -e ECOSYSTEM -e ECOSYSTEM_LABEL="Ecosystem Gateway" -v /var/local/flows-gateway/flows.json:/home/pm2user/.node-red/flows.json -v /var/local/flows-gateway/ssl:/home/pm2user/ssl -e VAULT_TOKEN -v /var/local/consul/ssl:/etc/vault/ssl canzea/flows-gateway:0.1.33
+    -e ECOSYSTEM -e ECOSYSTEM_LABEL="Ecosystem Gateway" -v /var/local/flows-gateway/flows.json:/home/pm2user/.node-red/flows.json -v /var/local/flows-gateway/ssl:/home/pm2user/ssl -e VAULT_TOKEN -v /var/local/consul/ssl:/etc/vault/ssl canzea/flows-gateway:0.1.35
 
 systemctl restart flows-gateway
 
