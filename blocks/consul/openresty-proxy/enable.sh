aaa85f71a2e34c49b91d69a3a24259a0e69a76bc
ikethecoder <ikethecoder@canzea.com>
2018-11-24 00:47:28 -0800

    openresty expose for consul

--- blocks/consul/openresty-proxy/enable.sh
+++ blocks/consul/openresty-proxy/enable.sh
@@ -1,6 +1,6 @@
 
 
-yes | cp -f $CATALOG_LOCATION/blocks/consul/openresty-proxy/config/consul_oidc.service /etc/systemd/system/multi-user.target.wants/.
+yes | cp -f blocks/consul/openresty-proxy/config/consul_oidc.service /etc/systemd/system/multi-user.target.wants/.
 
 systemctl daemon-reload
 
