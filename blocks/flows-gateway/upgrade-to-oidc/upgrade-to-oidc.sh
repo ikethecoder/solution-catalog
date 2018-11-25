25042503ac2b47c89709d3d818a54a0c17ef87c1
ikethecoder <ikethecoder@canzea.com>
2018-11-24 20:39:16 -0800

    flows-gateway upgrade-to-oidc use vlan0

--- blocks/flows-gateway/upgrade-to-oidc/upgrade-to-oidc.sh
+++ blocks/flows-gateway/upgrade-to-oidc/upgrade-to-oidc.sh
@@ -2,7 +2,7 @@
 
 docker rm -f flows-gateway || true
 
-mkdir -p /var/local/flows-gateway/projects
+mkdir -p /var/local/flows-gateway/projects && chown pm2user:pm2user /var/local/flows-gateway/projects
 
 docker create --name flows-gateway --net=vlan0 -p 8000:8000 \
     -e adminAuth=oauth2 \
@@ -14,7 +14,8 @@ docker create --name flows-gateway --net=vlan0 -p 8000:8000 \
     -e ADMIN_OAUTH2_CLIENT_SECRET=$OAUTH_CLIENTS_GITEA_CLIENT_SECRET \
     -e ADMIN_OAUTH2_ISSUER=$OAUTH_CLIENTS_GITEA_OIDC_ISSUER \
     -e ADMIN_OAUTH2_CALLBACK="https://$ES_DOMAIN/gwadmin/auth/strategy/callback" \
-    -e ECOSYSTEM -e ECOSYSTEM_LABEL="Ecosystem Gateway" -v /var/local/flows-gateway/projects:/home/pm2user/.node-red/projects -v /var/local/flows-gateway/flows.json:/home/pm2user/.node-red/flows.json -v /var/local/flows-gateway/ssl:/home/pm2user/ssl -e VAULT_TOKEN -v /var/local/consul/ssl:/etc/vault/ssl canzea/flows-gateway:0.1.35
+    -e ECOSYSTEM -e ECOSYSTEM_LABEL="Ecosystem Gateway" -v /var/local/flows-gateway/projects:/home/pm2user/.node-red/projects -v /var/local/flows-gateway/ssl:/home/pm2user/ssl -e VAULT_TOKEN -v /var/local/consul/ssl:/etc/vault/ssl canzea/flows-gateway:0.1.35
 
 systemctl restart flows-gateway
 
+# -v /var/local/flows-gateway/flows.json:/home/pm2user/.node-red/flows.json
