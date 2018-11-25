228704798363c0cb7e422f5e680d980709566381
ikethecoder <ikethecoder@canzea.com>
2018-11-25 01:29:04 -0800

    gocd-agent update

--- blocks/gocd-agent/first-config/first-config.sh
+++ blocks/gocd-agent/first-config/first-config.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+mkdir -p /var/local/gocd/home
+
 canzea --lifecycle=wire \
   --solution=ecosystem \
   --action=config \
@@ -12,6 +14,6 @@ canzea --lifecycle=wire \
   --action=config \
   --args='{"source":"{{CATALOG_LOCATION}}/blocks/gocd-agent/first-config/config/.git-credentials.mustache","target":"/var/local/gocd/home/.git-credentials","instanceId":"{{HOSTNAME}}","solution":"gocd-agent"}'
 
-chmod 644 /var/go/.git*
+chmod 644 /var/local/gocd/home/.git*
 
 systemctl stop gocd-agent && systemctl start gocd-agent
