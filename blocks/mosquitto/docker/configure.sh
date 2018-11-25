4f0e73b87504e94bc6151481605c3ab413b88ac6
ikethecoder <ikethecoder@canzea.com>
2018-11-24 19:44:13 -0800

    add mosquitto block

--- blocks/mosquitto/docker/configure.sh
+++ blocks/mosquitto/docker/configure.sh
@@ -1,7 +1,7 @@
 
 mkdir -p /var/local/mosquitto/config
 
-yes | cp blocks/mosquitto/docker/config/config.yaml /var/local/mosquitto/config/config.yaml
+yes | cp blocks/mosquitto/docker/config/mosquitto.conf /var/local/mosquitto/config/mosquitto.conf
 
 docker create --net=vlan0 --name mosquitto -p 1883:1883 -p 9001:9001 -v mosquitto.conf:/mosquitto/config/mosquitto.conf eclipse-mosquitto:1.5.4
 
