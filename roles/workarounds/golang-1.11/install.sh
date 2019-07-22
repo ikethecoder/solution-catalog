224a3faadd41fecddff7ff31693edb1620a9a3c7
Canzea Technologies <aidan.cope@canzea.com>
2019-07-22 22:56:06 +0000

    Add new golang 1.11

---
+++ roles/workarounds/golang-1.11/install.sh
@@ -0,0 +1,12 @@
+yum -y erase go
+
+rm -rf /usr/local/bin/go
+
+# Remove any previous version
+rm -rf /usr/local/go
+
+curl -L -s -O https://storage.googleapis.com/golang/go1.11.9.linux-amd64.tar.gz
+
+tar -C /usr/local -xzf go1.11.9.linux-amd64.tar.gz
+
+ln -s /usr/local/go/bin/go /usr/local/bin/go
